fe191c08abccc7f7b3694cbc016847575f5f2e77
Daniel <923daniel@naver.com>
2024-07-08 14:55:04 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/wad/seoul_nolgoat/SeoulNolgoatApplication.java
@@ -0,0 +1,13 @@
+package wad.seoul_nolgoat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulNolgoatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoulNolgoatApplication.class, args);
+	}
+
+}
