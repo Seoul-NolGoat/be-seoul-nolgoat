e407e12ed0e72a27cf37714a48015c6fbfe022a0
yonghyeonpark <pyh4616@naver.com>
2024-07-13 21:47:40 +0900

    feat: Review 도메인에 대한 컨트롤러, Dto 틀 구현

---
+++ src/main/java/wad/seoul_nolgoat/web/review/ReviewController.java
@@ -0,0 +1,7 @@
+package wad.seoul_nolgoat.web.review;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReviewController {
+}
---
+++ src/main/java/wad/seoul_nolgoat/web/review/dto/request/ReviewSave.java
@@ -0,0 +1,4 @@
+package wad.seoul_nolgoat.web.review.dto.request;
+
+public class ReviewSave {
+}
