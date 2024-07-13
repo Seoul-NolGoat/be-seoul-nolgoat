436121b08885b4d3e9195344478a3d08b0f2b648
yonghyeonpark <pyh4616@naver.com>
2024-07-13 21:41:03 +0900

    feat: Bookmark domain 구현
    - 엔터티 클래스
    - 리포지토리

---
+++ src/main/java/wad/seoul_nolgoat/domain/bookmark/Bookmark.java
@@ -0,0 +1,22 @@
+package wad.seoul_nolgoat.domain.bookmark;
+
+import jakarta.persistence.*;
+import wad.seoul_nolgoat.domain.BaseTimeEntity;
+import wad.seoul_nolgoat.domain.store.Store;
+import wad.seoul_nolgoat.domain.user.User;
+
+@Entity
+public class Bookmark extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+}
