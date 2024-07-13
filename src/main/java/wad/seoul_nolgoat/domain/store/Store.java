ac9f30abf8d9a7a08cc11411fe09566831188faf
yonghyeonpark <pyh4616@naver.com>
2024-07-13 21:39:34 +0900

    feat: Store domain 구현
    - 엔터티 클래스
    - 리포지토리
    - Type enum

---
+++ src/main/java/wad/seoul_nolgoat/domain/store/Store.java
@@ -0,0 +1,33 @@
+package wad.seoul_nolgoat.domain.store;
+
+import jakarta.persistence.*;
+import wad.seoul_nolgoat.domain.BaseTimeEntity;
+import wad.seoul_nolgoat.domain.review.Review;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Store extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Type type;
+
+    private String name;
+    private String category;
+    private String management_number;
+    private String phoneNumber;
+    private String lot_address;
+    private String road_address;
+    private double latitude;
+    private double longitude;
+    private double kakaoAverageGrade;
+    private double noloatAverageGrade;
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.REMOVE)
+    private List<Review> reviews = new ArrayList<>();
+}
---
+++ src/main/java/wad/seoul_nolgoat/domain/store/Type.java
@@ -0,0 +1,10 @@
+package wad.seoul_nolgoat.domain.store;
+
+public enum Type {
+
+    RESTAURANT,
+    CAFE,
+    PCROOM,
+    KARAOKE,
+    BILLIARD
+}
