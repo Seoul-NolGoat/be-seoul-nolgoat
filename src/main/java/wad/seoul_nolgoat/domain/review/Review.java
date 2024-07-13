68b4b935b89b602673264c68d46f68efe5974197
yonghyeonpark <pyh4616@naver.com>
2024-07-13 21:40:28 +0900

    feat: Review domain 구현
    - 엔터티 클래스
    - 리포지토리

---
+++ src/main/java/wad/seoul_nolgoat/domain/review/Review.java
@@ -0,0 +1,25 @@
+package wad.seoul_nolgoat.domain.review;
+
+import jakarta.persistence.*;
+import wad.seoul_nolgoat.domain.BaseTimeEntity;
+import wad.seoul_nolgoat.domain.store.Store;
+import wad.seoul_nolgoat.domain.user.User;
+
+@Entity
+public class Review extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private double grade;
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+}
---
+++ src/main/java/wad/seoul_nolgoat/domain/review/ReviewRepository.java
@@ -0,0 +1,6 @@
+package wad.seoul_nolgoat.domain.review;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
