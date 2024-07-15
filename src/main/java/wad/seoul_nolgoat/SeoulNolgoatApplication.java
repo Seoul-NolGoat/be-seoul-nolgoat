2cf90b9b64192c15ac2a8b113d0930488cb98dbb
Daniel <923daniel@naver.com>
2024-07-15 13:28:26 +0900

    chore: application.yml 파일에 config 파일 추가
    - kakao-config.yml
    - seoul-config.yml

--- src/main/java/wad/seoul_nolgoat/SeoulNolgoatApplication.java
+++ src/main/java/wad/seoul_nolgoat/SeoulNolgoatApplication.java
@@ -2,12 +2,19 @@ package wad.seoul_nolgoat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SeoulNolgoatApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SeoulNolgoatApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SeoulNolgoatApplication.class, args);
+    }
 
+    // "No beans of 'RestTemplate' type found" 오류 해결
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
