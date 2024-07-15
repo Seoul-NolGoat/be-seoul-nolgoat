3482a7e3c0e5c64bfd4f268d15e2b1eb7be4d6a6
Daniel <923daniel@naver.com>
2024-07-15 13:35:01 +0900

    feat: Store 엔터티 클래스에 업데이트 메서드 구현

--- src/main/java/wad/seoul_nolgoat/domain/store/Store.java
+++ src/main/java/wad/seoul_nolgoat/domain/store/Store.java
@@ -1,8 +1,7 @@
 package wad.seoul_nolgoat.domain.store;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import wad.seoul_nolgoat.domain.BaseTimeEntity;
 import wad.seoul_nolgoat.domain.review.Review;
 
@@ -10,7 +9,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Store extends BaseTimeEntity {
 
     @Id
@@ -18,19 +20,35 @@ public class Store extends BaseTimeEntity {
     private Long id;
 
     @Enumerated(EnumType.STRING)
-    private Type type;
+    private StoreType storeType;
 
     private String name;
     private String category;
-    private String management_number;
+    private String managementNumber;
     private String phoneNumber;
-    private String lot_address;
-    private String road_address;
+    private String lotAddress;
+    private String roadAddress;
     private double latitude;
     private double longitude;
     private double kakaoAverageGrade;
-    private double noloatAverageGrade;
+    private double nolgoatAverageGrade;
+    private String placeUrl;
 
     @OneToMany(mappedBy = "store", cascade = CascadeType.REMOVE)
     private List<Review> reviews = new ArrayList<>();
+
+    public void updateCoordinates(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public void updateAdditionalInfo(String category, String phoneNumber, String placeUrl) {
+        this.category = category;
+        this.phoneNumber = phoneNumber;
+        this.placeUrl = placeUrl;
+    }
+
+    public void updateKakaoAverageGrade(double kakaoAverageGrade) {
+        this.kakaoAverageGrade = kakaoAverageGrade;
+    }
 }
