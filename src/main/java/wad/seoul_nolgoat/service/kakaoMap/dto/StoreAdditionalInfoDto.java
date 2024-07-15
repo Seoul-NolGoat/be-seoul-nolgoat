8934dfff6239cd477ebf62d4d816b2db44bde45e
Daniel <923daniel@naver.com>
2024-07-15 13:39:19 +0900

    feat: Store 추가 정보 반환을 위한 StoreAdditionalInfoDto 구현

---
+++ src/main/java/wad/seoul_nolgoat/service/kakaoMap/dto/StoreAdditionalInfoDto.java
@@ -0,0 +1,13 @@
+package wad.seoul_nolgoat.service.kakaoMap.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class StoreAdditionalInfoDto {
+
+    private String category;
+    private String phoneNumber;
+    private String placeUrl;
+}
