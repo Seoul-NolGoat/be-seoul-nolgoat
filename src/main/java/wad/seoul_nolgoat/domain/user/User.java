1b683534386232dd87ebc1e9d1181cd9099c02c8
yonghyeonpark <pyh4616@naver.com>
2024-07-17 04:09:51 +0900

    feat: User 클래스에 필드 추가 및 생성자, 메서드 구현
    - soft delete 구현을 위한 isDeleted 필드 추가
    - update 메서드 구현
    - delete 메서드 구현

--- src/main/java/wad/seoul_nolgoat/domain/user/User.java
+++ src/main/java/wad/seoul_nolgoat/domain/user/User.java
@@ -2,11 +2,13 @@ package wad.seoul_nolgoat.domain.user;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import wad.seoul_nolgoat.domain.BaseTimeEntity;
 
 import java.time.LocalDate;
 
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class User extends BaseTimeEntity {
@@ -24,4 +26,33 @@ public class User extends BaseTimeEntity {
     private String profileImage;
     private String gender;
     private LocalDate birthDate;
+    private boolean isDeleted;
+
+    public User(
+            String loginId,
+            String password,
+            String nickname,
+            String profileImage,
+            String gender,
+            LocalDate birthDate) {
+        this.loginId = loginId;
+        this.password = password;
+        this.nickname = nickname;
+        this.profileImage = profileImage;
+        this.gender = gender;
+        this.birthDate = birthDate;
+    }
+
+    public void update(
+            String password,
+            String nickname,
+            String profileImage) {
+        this.password = password;
+        this.nickname = nickname;
+        this.profileImage = profileImage;
+    }
+
+    public void delete() {
+        this.isDeleted = true;
+    }
 }
