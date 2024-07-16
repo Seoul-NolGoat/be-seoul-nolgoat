3f1bb7e33b9ce70501a3da93af4e570feafd384c
yonghyeonpark <pyh4616@naver.com>
2024-07-17 04:18:44 +0900

    feat: User 객체 생성을 위한 Dto 구현

--- src/main/java/wad/seoul_nolgoat/web/user/dto/request/UserSaveDto.java
+++ src/main/java/wad/seoul_nolgoat/web/user/dto/request/UserSaveDto.java
@@ -1,4 +1,18 @@
 package wad.seoul_nolgoat.web.user.dto.request;
 
-public class UserSave {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@RequiredArgsConstructor
+public class UserSaveDto {
+
+    private final String loginId;
+    private final String password;
+    private final String nickname;
+    private final String profileImage;
+    private final String gender;
+    private final LocalDate birthDate;
 }
