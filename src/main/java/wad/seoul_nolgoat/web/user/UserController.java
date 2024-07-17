f6543e87f53b53685d03894565a41232cf41a706
yonghyeonpark <pyh4616@naver.com>
2024-07-17 15:26:28 +0900

    feat: User CRUD API 구현
    - createUser
    - showUserById
    - update
    - deleteById

--- src/main/java/wad/seoul_nolgoat/web/user/UserController.java
+++ src/main/java/wad/seoul_nolgoat/web/user/UserController.java
@@ -1,7 +1,56 @@
 package wad.seoul_nolgoat.web.user;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+import wad.seoul_nolgoat.service.user.UserService;
+import wad.seoul_nolgoat.web.user.dto.request.UserSaveDto;
+import wad.seoul_nolgoat.web.user.dto.request.UserUpdateDto;
+import wad.seoul_nolgoat.web.user.dto.response.UserDetailsDto;
 
+import java.net.URI;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
 @RestController
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<Void> createUser(@RequestBody UserSaveDto userSaveDto, UriComponentsBuilder uriComponentsBuilder) {
+        Long userId = userService.save(userSaveDto);
+        URI location = uriComponentsBuilder.path("/api/users/{userId}")
+                .buildAndExpand(userId)
+                .toUri();
+
+        return ResponseEntity
+                .created(location)
+                .build();
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDetailsDto> showUserById(@PathVariable Long userId) {
+        return ResponseEntity
+                .ok(userService.findById(userId));
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<Void> update(@PathVariable Long userId, @RequestBody UserUpdateDto userUpdateDto) {
+        userService.update(userId, userUpdateDto);
+
+        return ResponseEntity
+                .noContent()
+                .build();
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long userId) {
+        userService.deleteById(userId);
+
+        return ResponseEntity
+                .noContent()
+                .build();
+    }
 }
