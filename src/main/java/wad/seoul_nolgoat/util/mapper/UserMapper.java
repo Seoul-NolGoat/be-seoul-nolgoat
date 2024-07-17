bda2254247927d0ddc57716da86c3a69996f1ea3
yonghyeonpark <pyh4616@naver.com>
2024-07-17 14:45:24 +0900

    feat: User의 엔터티 객체, Dto의 상호 변환을 위한 Mapper 클래스 구현

---
+++ src/main/java/wad/seoul_nolgoat/util/mapper/UserMapper.java
@@ -0,0 +1,29 @@
+package wad.seoul_nolgoat.util.mapper;
+
+import wad.seoul_nolgoat.domain.user.User;
+import wad.seoul_nolgoat.web.user.dto.request.UserSaveDto;
+import wad.seoul_nolgoat.web.user.dto.response.UserDetailsDto;
+
+public class UserMapper {
+
+    public static User toEntity(UserSaveDto userSaveDto) {
+        return new User(
+                userSaveDto.getLoginId(),
+                userSaveDto.getPassword(),
+                userSaveDto.getNickname(),
+                userSaveDto.getProfileImage(),
+                userSaveDto.getGender(),
+                userSaveDto.getBirthDate()
+        );
+    }
+
+    public static UserDetailsDto toUserDetailsDto(User user) {
+        return new UserDetailsDto(
+                user.getLoginId(),
+                user.getNickname(),
+                user.getProfileImage(),
+                user.getGender(),
+                user.getBirthDate()
+        );
+    }
+}
