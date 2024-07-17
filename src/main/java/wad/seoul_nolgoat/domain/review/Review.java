4f61c4d64a33215765208877c8dc63b56b374387
yonghyeonpark <pyh4616@naver.com>
2024-07-17 18:32:01 +0900

    feat: Review 클래스에 생성자 및 update 메서드 구현

--- src/main/java/wad/seoul_nolgoat/domain/review/Review.java
+++ src/main/java/wad/seoul_nolgoat/domain/review/Review.java
@@ -2,11 +2,13 @@ package wad.seoul_nolgoat.domain.review;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import wad.seoul_nolgoat.domain.BaseTimeEntity;
 import wad.seoul_nolgoat.domain.store.Store;
 import wad.seoul_nolgoat.domain.user.User;
 
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Review extends BaseTimeEntity {
@@ -25,4 +27,20 @@ public class Review extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "store_id")
     private Store store;
+
+    public Review(
+            double grade,
+            String content,
+            User user,
+            Store store) {
+        this.grade = grade;
+        this.content = content;
+        this.user = user;
+        this.store = store;
+    }
+
+    public void update(double grade, String content) {
+        this.grade = grade;
+        this.content = content;
+    }
 }
