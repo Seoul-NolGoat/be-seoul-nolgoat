1b6a38bada98a2e8a3066901304d00f966f3556d
yonghyeonpark <pyh4616@naver.com>
2024-07-17 14:46:00 +0900

    feat: User 엔터티 기본 CRUD 구현
    - save
    - findById
    - update
    - deleteById

--- src/main/java/wad/seoul_nolgoat/service/user/UserService.java
+++ src/main/java/wad/seoul_nolgoat/service/user/UserService.java
@@ -1,7 +1,41 @@
 package wad.seoul_nolgoat.service.user;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import wad.seoul_nolgoat.domain.user.UserRepository;
+import wad.seoul_nolgoat.util.mapper.UserMapper;
+import wad.seoul_nolgoat.web.user.dto.request.UserSaveDto;
+import wad.seoul_nolgoat.web.user.dto.request.UserUpdateDto;
+import wad.seoul_nolgoat.web.user.dto.response.UserDetailsDto;
 
+@RequiredArgsConstructor
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public Long save(UserSaveDto userSaveDto) {
+        return userRepository.save(UserMapper.toEntity(userSaveDto)).getId();
+    }
+
+    public UserDetailsDto findById(Long userId) {
+        return UserMapper.toUserDetailsDto(userRepository.findById(userId).get());
+    }
+
+    @Transactional
+    public void update(Long userId, UserUpdateDto userUpdateDto) {
+        userRepository.findById(userId).get()
+                .update(
+                        userUpdateDto.getPassword(),
+                        userUpdateDto.getNickname(),
+                        userUpdateDto.getProfileImage()
+                );
+    }
+
+    @Transactional
+    public void deleteById(Long userId) {
+        userRepository.findById(userId).get()
+                .delete();
+    }
 }
