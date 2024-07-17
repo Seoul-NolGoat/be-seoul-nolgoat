863bdca1dc9d250fbe350d20cc5a7f8bd4830112
yonghyeonpark <pyh4616@naver.com>
2024-07-17 23:30:17 +0900

    feat: Bookmark 클래스에 생성자 구현

--- src/main/java/wad/seoul_nolgoat/domain/bookmark/Bookmark.java
+++ src/main/java/wad/seoul_nolgoat/domain/bookmark/Bookmark.java
@@ -2,11 +2,13 @@ package wad.seoul_nolgoat.domain.bookmark;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import wad.seoul_nolgoat.domain.BaseTimeEntity;
 import wad.seoul_nolgoat.domain.store.Store;
 import wad.seoul_nolgoat.domain.user.User;
 
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Bookmark extends BaseTimeEntity {
@@ -22,4 +24,9 @@ public class Bookmark extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "store_id")
     private Store store;
+
+    public Bookmark(User user, Store store) {
+        this.user = user;
+        this.store = store;
+    }
 }
