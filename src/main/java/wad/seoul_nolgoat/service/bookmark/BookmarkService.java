4595e2954bee16b09464c923aacbab6cb8fb0503
yonghyeonpark <pyh4616@naver.com>
2024-07-17 23:39:56 +0900

    feat: Bookmark 엔터티 기본 CRUD 구현
    - save
    - deleteById

--- src/main/java/wad/seoul_nolgoat/service/bookmark/BookmarkService.java
+++ src/main/java/wad/seoul_nolgoat/service/bookmark/BookmarkService.java
@@ -1,7 +1,35 @@
 package wad.seoul_nolgoat.service.bookmark;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import wad.seoul_nolgoat.domain.bookmark.Bookmark;
+import wad.seoul_nolgoat.domain.bookmark.BookmarkRepository;
+import wad.seoul_nolgoat.domain.store.Store;
+import wad.seoul_nolgoat.domain.store.StoreRepository;
+import wad.seoul_nolgoat.domain.user.User;
+import wad.seoul_nolgoat.domain.user.UserRepository;
 
+@RequiredArgsConstructor
 @Service
 public class BookmarkService {
+
+    private final BookmarkRepository bookmarkRepository;
+    private final UserRepository userRepository;
+    private final StoreRepository storeRepository;
+
+    public Long save(Long userId, Long storeId) {
+        User user = userRepository.findById(userId).get();
+        Store store = storeRepository.findById(storeId).get();
+
+        return bookmarkRepository.save(
+                new Bookmark(
+                        user,
+                        store
+                )
+        ).getId();
+    }
+
+    public void deleteById(Long bookmarkId) {
+        bookmarkRepository.deleteById(bookmarkId);
+    }
 }
