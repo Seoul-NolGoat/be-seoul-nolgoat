b64a2e9a3c8b409162ca8e0e45e20f28889a2ae5
Daniel <923daniel@naver.com>
2024-07-18 02:30:45 +0900

    feat: Store 엔터티 기본 CRUD 구현

--- src/main/java/wad/seoul_nolgoat/service/store/StoreService.java
+++ src/main/java/wad/seoul_nolgoat/service/store/StoreService.java
@@ -1,7 +1,46 @@
 package wad.seoul_nolgoat.service.store;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import wad.seoul_nolgoat.domain.store.Store;
+import wad.seoul_nolgoat.domain.store.StoreRepository;
+import wad.seoul_nolgoat.service.store.dto.StoreUpdateDto;
+import wad.seoul_nolgoat.util.mapper.StoreMapper;
+import wad.seoul_nolgoat.web.store.dto.response.StoreDetailsDto;
 
+@RequiredArgsConstructor
 @Service
 public class StoreService {
+
+    private final StoreRepository storeRepository;
+
+    public StoreDetailsDto findByStoreId(Long storeId) {
+        Store store = storeRepository.findById(storeId).get();
+
+        return StoreMapper.toStoreDetailsDto(store);
+    }
+
+    @Transactional
+    public void update(StoreUpdateDto storeUpdateDto) {
+        Store store = storeRepository.findById(storeUpdateDto.getId()).get();
+        store.update(
+                storeUpdateDto.getName(),
+                storeUpdateDto.getCategory(),
+                storeUpdateDto.getManagementNumber(),
+                storeUpdateDto.getPhoneNumber(),
+                storeUpdateDto.getLotAddress(),
+                storeUpdateDto.getRoadAddress(),
+                storeUpdateDto.getLatitude(),
+                storeUpdateDto.getLongitude(),
+                storeUpdateDto.getKakaoAverageGrade(),
+                storeUpdateDto.getPlaceUrl()
+        );
+    }
+
+    @Transactional
+    public void deleteById(Long storeId) {
+        Store store = storeRepository.findById(storeId).get();
+        store.delete();
+    }
 }
--- src/main/java/wad/seoul_nolgoat/web/store/StoreController.java
+++ src/main/java/wad/seoul_nolgoat/web/store/StoreController.java
@@ -1,7 +1,24 @@
 package wad.seoul_nolgoat.web.store;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import wad.seoul_nolgoat.service.store.StoreService;
+import wad.seoul_nolgoat.web.store.dto.response.StoreDetailsDto;
 
+@RequiredArgsConstructor
+@RequestMapping("/api/stores")
 @RestController
 public class StoreController {
+
+    private final StoreService storeService;
+
+    @GetMapping("/{storeId}")
+    public ResponseEntity<StoreDetailsDto> showStoreByStoreId(@PathVariable Long storeId) {
+        return ResponseEntity
+                .ok(storeService.findByStoreId(storeId));
+    }
 }
