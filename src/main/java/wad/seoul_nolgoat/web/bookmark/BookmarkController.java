8d56b8816173874245d34c76075252e7b315dabc
yonghyeonpark <pyh4616@naver.com>
2024-07-17 23:41:27 +0900

    feat: Bookmark CRUD API 구현
    - createBookmark
    - deleteById

--- src/main/java/wad/seoul_nolgoat/web/bookmark/BookmarkController.java
+++ src/main/java/wad/seoul_nolgoat/web/bookmark/BookmarkController.java
@@ -1,7 +1,41 @@
 package wad.seoul_nolgoat.web.bookmark;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+import wad.seoul_nolgoat.service.bookmark.BookmarkService;
 
+import java.net.URI;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/bookmarks")
 @RestController
 public class BookmarkController {
+
+    private final BookmarkService bookmarkService;
+
+    @PostMapping("/{userId}{storeId}")
+    public ResponseEntity<Void> createBookmark(
+            @PathVariable Long userId,
+            @PathVariable Long storeId,
+            UriComponentsBuilder uriComponentsBuilder) {
+        Long bookmarkId = bookmarkService.save(userId, storeId);
+        URI location = uriComponentsBuilder.path("/api/bookmarks/{bookmarkId}")
+                .buildAndExpand(bookmarkId)
+                .toUri();
+
+        return ResponseEntity
+                .created(location)
+                .build();
+    }
+
+    @DeleteMapping("/{bookmarkId}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long bookmarkId) {
+        bookmarkService.deleteById(bookmarkId);
+
+        return ResponseEntity
+                .noContent()
+                .build();
+    }
 }
