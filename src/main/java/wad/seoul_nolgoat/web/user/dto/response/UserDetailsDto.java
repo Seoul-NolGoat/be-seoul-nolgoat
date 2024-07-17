c4676304bc3eb222c48fa118c38013df56732336
yonghyeonpark <pyh4616@naver.com>
2024-07-17 14:25:16 +0900

    feat: User 단건 조회를 위한 Dto 구현

--- src/main/java/wad/seoul_nolgoat/web/user/dto/response/UserDetailsDto.java
+++ src/main/java/wad/seoul_nolgoat/web/user/dto/response/UserDetailsDto.java
@@ -1,4 +1,17 @@
 package wad.seoul_nolgoat.web.user.dto.response;
 
-public class UserResponse {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@RequiredArgsConstructor
+public class UserDetailsDto {
+
+    private final String loginId;
+    private final String nickname;
+    private final String profileImage;
+    private final String gender;
+    private final LocalDate birthDate;
 }
