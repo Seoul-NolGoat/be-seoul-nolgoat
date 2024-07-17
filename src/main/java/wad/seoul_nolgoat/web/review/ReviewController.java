fbeef57348fe4d9908d3161cdaac6ccc3cd9aa51
yonghyeonpark <pyh4616@naver.com>
2024-07-17 18:49:00 +0900

    feat: Review CRUD API 구현
    - createReview
    - showReviewsByUserId
    - showReviewsByStoreId
    - update
    - deleteById

--- src/main/java/wad/seoul_nolgoat/web/review/ReviewController.java
+++ src/main/java/wad/seoul_nolgoat/web/review/ReviewController.java
@@ -1,7 +1,71 @@
 package wad.seoul_nolgoat.web.review;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+import wad.seoul_nolgoat.service.review.ReviewService;
+import wad.seoul_nolgoat.web.review.dto.request.ReviewSaveDto;
+import wad.seoul_nolgoat.web.review.dto.request.ReviewUpdateDto;
+import wad.seoul_nolgoat.web.review.dto.response.ReviewDetailsDto;
 
+import java.net.URI;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/reviews")
 @RestController
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @PostMapping("/{userId}/{storeId}")
+    public ResponseEntity<Void> createReview(
+            @PathVariable Long userId,
+            @PathVariable Long storeId,
+            @RequestBody ReviewSaveDto reviewSaveDto,
+            UriComponentsBuilder uriComponentsBuilder) {
+        Long reviewId = reviewService.save(
+                userId,
+                storeId,
+                reviewSaveDto
+        );
+        URI location = uriComponentsBuilder.path("/api/reviews/{reviewId}")
+                .buildAndExpand(reviewId)
+                .toUri();
+
+        return ResponseEntity
+                .created(location)
+                .build();
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<ReviewDetailsDto>> showReviewsByUserId(@PathVariable Long userId) {
+        return ResponseEntity
+                .ok(reviewService.findByUserId(userId));
+    }
+
+    @GetMapping("/{storeId}")
+    public ResponseEntity<List<ReviewDetailsDto>> showReviewsByStoreId(@PathVariable Long storeId) {
+        return ResponseEntity
+                .ok(reviewService.findByStoreId(storeId));
+    }
+
+    @PutMapping("/{reviewId}")
+    public ResponseEntity<Void> update(@PathVariable Long reviewId, @RequestBody ReviewUpdateDto reviewUpdateDto) {
+        reviewService.update(reviewId, reviewUpdateDto);
+
+        return ResponseEntity
+                .noContent()
+                .build();
+    }
+
+    @DeleteMapping("/{reviewId}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long reviewId) {
+        reviewService.deleteById(reviewId);
+
+        return ResponseEntity
+                .noContent()
+                .build();
+    }
 }
