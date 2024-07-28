33512a4b002761c62a82d22adcdcf0b0c04b59c0
yonghyeonpark <pyh4616@naver.com>
2024-07-28 15:21:21 +0900

    feat: 조건에 따른 조합들을 받는 API 구현

---
+++ src/main/java/wad/seoul_nolgoat/web/search/SearchController.java
@@ -0,0 +1,27 @@
+package wad.seoul_nolgoat.web.search;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import wad.seoul_nolgoat.service.search.SearchService;
+import wad.seoul_nolgoat.web.search.dto.request.SearchConditionDto;
+import wad.seoul_nolgoat.web.search.dto.response.CombinationDto;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/search")
+@RestController
+public class SearchController {
+
+    private final SearchService searchService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<CombinationDto>> searchAll(@ModelAttribute SearchConditionDto searchConditionDto) {
+        return ResponseEntity
+                .ok(searchService.searchAll(searchConditionDto));
+    }
+}
