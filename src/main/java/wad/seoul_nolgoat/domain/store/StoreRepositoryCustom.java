04c31ec7848fa82054ee2271b95cbca1f3d18fbb
yonghyeonpark <pyh4616@naver.com>
2024-07-28 13:11:48 +0900

    feat: querydsl 사용을 위한 별도의 StoreRepository 인터페이스 구현

---
+++ src/main/java/wad/seoul_nolgoat/domain/store/StoreRepositoryCustom.java
@@ -0,0 +1,28 @@
+package wad.seoul_nolgoat.domain.store;
+
+import wad.seoul_nolgoat.service.search.dto.StoreForDistanceSortDto;
+import wad.seoul_nolgoat.service.search.dto.StoreForGradeSortDto;
+import wad.seoul_nolgoat.web.search.dto.CoordinateDto;
+
+import java.util.List;
+
+public interface StoreRepositoryCustom {
+
+    List<StoreForDistanceSortDto> findByRadiusRangeAndCategoryForDistanceSort(
+            CoordinateDto startCoordinate,
+            double radiusRange,
+            String category
+    );
+
+    List<StoreForGradeSortDto> findByRadiusRangeAndCategoryForKakaoGradeSort(
+            CoordinateDto startCoordinate,
+            double radiusRange,
+            String category
+    );
+
+    List<StoreForGradeSortDto> findByRadiusRangeAndCategoryForNolgoatGradeSort(
+            CoordinateDto startCoordinate,
+            double radiusRange,
+            String category
+    );
+}
