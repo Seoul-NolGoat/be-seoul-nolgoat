6ae9cfcca9f670bc268a34710b28e3a9e147412e
yonghyeonpark <pyh4616@naver.com>
2024-07-28 13:14:13 +0900

    refactor: querydsl Projections.constructor에 사용하기 위해 기본 생성자 추가

--- src/main/java/wad/seoul_nolgoat/web/search/dto/CoordinateDto.java
+++ src/main/java/wad/seoul_nolgoat/web/search/dto/CoordinateDto.java
@@ -1,10 +1,13 @@
 package wad.seoul_nolgoat.web.search.dto;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CoordinateDto {
 
     private double latitude;
