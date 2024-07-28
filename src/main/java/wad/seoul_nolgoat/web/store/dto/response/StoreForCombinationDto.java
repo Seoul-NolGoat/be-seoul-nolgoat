ec7758bf1fc7ed4a0d9fd0cfaba9858534416068
yonghyeonpark <pyh4616@naver.com>
2024-07-28 14:06:35 +0900

    feat: 최종 응답에 사용하기 위한 Store Dto 구현

---
+++ src/main/java/wad/seoul_nolgoat/web/store/dto/response/StoreForCombinationDto.java
@@ -0,0 +1,14 @@
+package wad.seoul_nolgoat.web.store.dto.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import wad.seoul_nolgoat.web.search.dto.CoordinateDto;
+
+@Getter
+@RequiredArgsConstructor
+public class StoreForCombinationDto {
+
+    private final Long id;
+    private final String name;
+    private final CoordinateDto coordinate;
+}
