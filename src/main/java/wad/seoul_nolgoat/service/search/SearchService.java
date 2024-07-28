c9e0f3ad57c924e9b886b1ee176bb7687797ceb0
yonghyeonpark <pyh4616@naver.com>
2024-07-28 15:18:09 +0900

    feat: 검색 조건에 따라 Store를 필터링하고 정렬하여 최종 조합들을 반환하는 기능 구현

---
+++ src/main/java/wad/seoul_nolgoat/service/search/SearchService.java
@@ -0,0 +1,216 @@
+package wad.seoul_nolgoat.service.search;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import wad.seoul_nolgoat.service.search.dto.SortConditionDto;
+import wad.seoul_nolgoat.service.search.filter.FilterService;
+import wad.seoul_nolgoat.service.search.sort.SortService;
+import wad.seoul_nolgoat.util.mapper.CombinationMapper;
+import wad.seoul_nolgoat.web.search.dto.request.SearchConditionDto;
+import wad.seoul_nolgoat.web.search.dto.response.CombinationDto;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class SearchService {
+
+    private final FilterService filterService;
+    private final SortService sortService;
+
+    public List<CombinationDto> searchAll(SearchConditionDto searchConditionDto) {
+        if (searchConditionDto.getCriteria().equals("distance")) {
+            return getCombinationsByDistance(searchConditionDto);
+        }
+        if (searchConditionDto.getCriteria().equals("kakaoGrade")) {
+            return getCombinationsByKakaoGrade(searchConditionDto);
+        }
+        if (searchConditionDto.getCriteria().equals("nolgoatGrade")) {
+            return getCombinationsByNolgoatGrade(searchConditionDto);
+        }
+        throw new RuntimeException();
+    }
+
+    private List<CombinationDto> getCombinationsByDistance(SearchConditionDto searchConditionDto) {
+        if (searchConditionDto.getCategories().size() == 3) {
+            return sortService.sortByDistance(
+                            new SortConditionDto<>(
+                                    searchConditionDto.getStartCoordinate(),
+                                    filterService.filterByRadiusRangeAndCategoryForDistanceSort(
+                                            searchConditionDto.getStartCoordinate(),
+                                            searchConditionDto.getRadiusRange(),
+                                            searchConditionDto.getCategories().get(0)
+                                    ),
+                                    filterService.filterByRadiusRangeAndCategoryForDistanceSort(
+                                            searchConditionDto.getStartCoordinate(),
+                                            searchConditionDto.getRadiusRange(),
+                                            searchConditionDto.getCategories().get(1)
+                                    ),
+                                    filterService.filterByRadiusRangeAndCategoryForDistanceSort(
+                                            searchConditionDto.getStartCoordinate(),
+                                            searchConditionDto.getRadiusRange(),
+                                            searchConditionDto.getCategories().get(2)
+                                    )
+                            )
+                    ).stream()
+                    .map(CombinationMapper::toCombinationDto)
+                    .toList();
+        }
+        if (searchConditionDto.getCategories().size() == 2) {
+            return sortService.sortByDistance(
+                            new SortConditionDto<>(
+                                    searchConditionDto.getStartCoordinate(),
+                                    filterService.filterByRadiusRangeAndCategoryForDistanceSort(
+                                            searchConditionDto.getStartCoordinate(),
+                                            searchConditionDto.getRadiusRange(),
+                                            searchConditionDto.getCategories().get(0)
+                                    ),
+                                    filterService.filterByRadiusRangeAndCategoryForDistanceSort(
+                                            searchConditionDto.getStartCoordinate(),
+                                            searchConditionDto.getRadiusRange(),
+                                            searchConditionDto.getCategories().get(1)
+                                    )
+                            )
+                    ).stream()
+                    .map(CombinationMapper::toCombinationDto)
+                    .toList();
+        }
+        if (searchConditionDto.getCategories().size() == 1) {
+            return sortService.sortByDistance(
+                            new SortConditionDto<>(
+                                    searchConditionDto.getStartCoordinate(),
+                                    filterService.filterByRadiusRangeAndCategoryForDistanceSort(
+                                            searchConditionDto.getStartCoordinate(),
+                                            searchConditionDto.getRadiusRange(),
+                                            searchConditionDto.getCategories().get(0)
+                                    )
+                            )
+                    ).stream()
+                    .map(CombinationMapper::toCombinationDto)
+                    .toList();
+        }
+        throw new RuntimeException();
+    }
+
+    private List<CombinationDto> getCombinationsByKakaoGrade(SearchConditionDto searchConditionDto) {
+        if (searchConditionDto.getCategories().size() == 3) {
+            return sortService.sortByGrade(
+                            new SortConditionDto<>(
+                                    searchConditionDto.getStartCoordinate(),
+                                    filterService.filterByRadiusRangeAndCategoryForKakaoGradeSort(
+                                            searchConditionDto.getStartCoordinate(),
+                                            searchConditionDto.getRadiusRange(),
+                                            searchConditionDto.getCategories().get(0)
+                                    ),
+                                    filterService.filterByRadiusRangeAndCategoryForKakaoGradeSort(
+                                            searchConditionDto.getStartCoordinate(),
+                                            searchConditionDto.getRadiusRange(),
+                                            searchConditionDto.getCategories().get(1)
+                                    ),
+                                    filterService.filterByRadiusRangeAndCategoryForKakaoGradeSort(
+                                            searchConditionDto.getStartCoordinate(),
+                                            searchConditionDto.getRadiusRange(),
+                                            searchConditionDto.getCategories().get(2)
+                                    )
+                            )
+                    ).stream()
+                    .map(CombinationMapper::toCombinationDto)
+                    .toList();
+        }
+        if (searchConditionDto.getCategories().size() == 2) {
+            return sortService.sortByGrade(
+                            new SortConditionDto<>(
+                                    searchConditionDto.getStartCoordinate(),
+                                    filterService.filterByRadiusRangeAndCategoryForKakaoGradeSort(
+                                            searchConditionDto.getStartCoordinate(),
+                                            searchConditionDto.getRadiusRange(),
+                                            searchConditionDto.getCategories().get(0)
+                                    ),
+                                    filterService.filterByRadiusRangeAndCategoryForKakaoGradeSort(
+                                            searchConditionDto.getStartCoordinate(),
+                                            searchConditionDto.getRadiusRange(),
+                                            searchConditionDto.getCategories().get(1)
+                                    )
+                            )
+                    ).stream()
+                    .map(CombinationMapper::toCombinationDto)
+                    .toList();
+        }
+        if (searchConditionDto.getCategories().size() == 1) {
+            return sortService.sortByGrade(
+                            new SortConditionDto<>(
+                                    searchConditionDto.getStartCoordinate(),
+                                    filterService.filterByRadiusRangeAndCategoryForKakaoGradeSort(
+                                            searchConditionDto.getStartCoordinate(),
+                                            searchConditionDto.getRadiusRange(),
+                                            searchConditionDto.getCategories().get(0)
+                                    )
+                            )
+                    ).stream()
+                    .map(CombinationMapper::toCombinationDto)
+                    .toList();
+        }
+        throw new RuntimeException();
+    }
+
+    private List<CombinationDto> getCombinationsByNolgoatGrade(SearchConditionDto searchConditionDto) {
+        if (searchConditionDto.getCategories().size() == 3) {
+            return sortService.sortByGrade(
+                            new SortConditionDto<>(
+                                    searchConditionDto.getStartCoordinate(),
+                                    filterService.filterByRadiusRangeAndCategoryForNolgoatGradeSort(
+                                            searchConditionDto.getStartCoordinate(),
+                                            searchConditionDto.getRadiusRange(),
+                                            searchConditionDto.getCategories().get(0)
+                                    ),
+                                    filterService.filterByRadiusRangeAndCategoryForNolgoatGradeSort(
+                                            searchConditionDto.getStartCoordinate(),
+                                            searchConditionDto.getRadiusRange(),
+                                            searchConditionDto.getCategories().get(1)
+                                    ),
+                                    filterService.filterByRadiusRangeAndCategoryForNolgoatGradeSort(
+                                            searchConditionDto.getStartCoordinate(),
+                                            searchConditionDto.getRadiusRange(),
+                                            searchConditionDto.getCategories().get(2)
+                                    )
+                            )
+                    ).stream()
+                    .map(CombinationMapper::toCombinationDto)
+                    .toList();
+        }
+        if (searchConditionDto.getCategories().size() == 2) {
+            return sortService.sortByGrade(
+                            new SortConditionDto<>(
+                                    searchConditionDto.getStartCoordinate(),
+                                    filterService.filterByRadiusRangeAndCategoryForNolgoatGradeSort(
+                                            searchConditionDto.getStartCoordinate(),
+                                            searchConditionDto.getRadiusRange(),
+                                            searchConditionDto.getCategories().get(0)
+                                    ),
+                                    filterService.filterByRadiusRangeAndCategoryForNolgoatGradeSort(
+                                            searchConditionDto.getStartCoordinate(),
+                                            searchConditionDto.getRadiusRange(),
+                                            searchConditionDto.getCategories().get(1)
+                                    )
+                            )
+                    ).stream()
+                    .map(CombinationMapper::toCombinationDto)
+                    .toList();
+        }
+        if (searchConditionDto.getCategories().size() == 1) {
+            return sortService.sortByGrade(
+                            new SortConditionDto<>(
+                                    searchConditionDto.getStartCoordinate(),
+                                    filterService.filterByRadiusRangeAndCategoryForNolgoatGradeSort(
+                                            searchConditionDto.getStartCoordinate(),
+                                            searchConditionDto.getRadiusRange(),
+                                            searchConditionDto.getCategories().get(0)
+                                    )
+                            )
+                    ).stream()
+                    .map(CombinationMapper::toCombinationDto)
+                    .toList();
+        }
+        throw new RuntimeException();
+    }
+}
