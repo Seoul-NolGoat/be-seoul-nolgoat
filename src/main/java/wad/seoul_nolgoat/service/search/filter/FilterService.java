e4f378c598856c4139ac00483f25d5e6ba576578
yonghyeonpark <pyh4616@naver.com>
2024-07-28 13:43:46 +0900

    feat: 조합 검색 조건을 통해 Store들을 필터링하는 기능 구현
    - 거리 정렬을 위한 StoreDto를 사용
    - 카카오 평점 정렬을 위한 StoreDto를 사용
    - NolGoat 평점 정렬을 위한 StoreDto를 사용

---
+++ src/main/java/wad/seoul_nolgoat/service/search/filter/FilterService.java
@@ -0,0 +1,50 @@
+package wad.seoul_nolgoat.service.search.filter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import wad.seoul_nolgoat.domain.store.StoreRepository;
+import wad.seoul_nolgoat.service.search.dto.StoreForDistanceSortDto;
+import wad.seoul_nolgoat.service.search.dto.StoreForGradeSortDto;
+import wad.seoul_nolgoat.web.search.dto.CoordinateDto;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class FilterService {
+
+    private final StoreRepository storeRepository;
+
+    public List<StoreForDistanceSortDto> filterByRadiusRangeAndCategoryForDistanceSort(
+            CoordinateDto startCoordinate,
+            double radiusRange,
+            String category) {
+        return storeRepository.findByRadiusRangeAndCategoryForDistanceSort(
+                startCoordinate,
+                radiusRange,
+                category
+        );
+    }
+
+    public List<StoreForGradeSortDto> filterByRadiusRangeAndCategoryForKakaoGradeSort(
+            CoordinateDto startCoordinate,
+            double radiusRange,
+            String category) {
+        return storeRepository.findByRadiusRangeAndCategoryForKakaoGradeSort(
+                startCoordinate,
+                radiusRange,
+                category
+        );
+    }
+
+    public List<StoreForGradeSortDto> filterByRadiusRangeAndCategoryForNolgoatGradeSort(
+            CoordinateDto startCoordinate,
+            double radiusRange,
+            String category) {
+        return storeRepository.findByRadiusRangeAndCategoryForNolgoatGradeSort(
+                startCoordinate,
+                radiusRange,
+                category
+        );
+    }
+}
