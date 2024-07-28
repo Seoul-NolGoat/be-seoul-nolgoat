d45e5c7f89632da46d965b8d4fedbd12c89fd30e
yonghyeonpark <pyh4616@naver.com>
2024-07-28 14:58:30 +0900

    feat: 거리, 평점 정렬을 위해 사용한 Store Dto들을 StoreForCombinationDto로 변환하는 기능 구현

--- src/main/java/wad/seoul_nolgoat/util/mapper/StoreMapper.java
+++ src/main/java/wad/seoul_nolgoat/util/mapper/StoreMapper.java
@@ -1,7 +1,10 @@
 package wad.seoul_nolgoat.util.mapper;
 
 import wad.seoul_nolgoat.domain.store.Store;
+import wad.seoul_nolgoat.service.search.dto.StoreForDistanceSortDto;
+import wad.seoul_nolgoat.service.search.dto.StoreForGradeSortDto;
 import wad.seoul_nolgoat.web.store.dto.response.StoreDetailsDto;
+import wad.seoul_nolgoat.web.store.dto.response.StoreForCombinationDto;
 
 public class StoreMapper {
 
@@ -23,6 +26,22 @@ public class StoreMapper {
                 store.getReviews().stream()
                         .map(ReviewMapper::toReviewDetailsForStoreDto)
                         .toList()
+        );
+    }
+
+    public static StoreForCombinationDto toStoreForCombinationDto(StoreForDistanceSortDto storeForDistanceSortDto) {
+        return new StoreForCombinationDto(
+                storeForDistanceSortDto.getId(),
+                storeForDistanceSortDto.getName(),
+                storeForDistanceSortDto.getCoordinate()
+        );
+    }
+
+    public static StoreForCombinationDto toStoreForCombinationDto(StoreForGradeSortDto storeForGradeSortDto) {
+        return new StoreForCombinationDto(
+                storeForGradeSortDto.getId(),
+                storeForGradeSortDto.getName(),
+                storeForGradeSortDto.getCoordinate()
         );
     }
 }
