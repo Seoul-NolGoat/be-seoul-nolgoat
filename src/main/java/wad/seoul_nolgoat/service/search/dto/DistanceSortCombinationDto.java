2cd1c813761991ba8478abbacfa2ed1758a7cbb7
horang <fnelclsrn123@gmail,com>
2024-07-30 17:36:09 +0900

    feat: Store를 평점 정렬, 거리 정렬하는 SortService 클래스 구현
    -Store 조합의 총 평점을 기준으로 정렬하는 기능 구현
    -Store 조합의 총 거리를 기준으로 정렬하는 기능 구현
    -TAmp Api를 호출해서 실제 보행자 이동거리를 기준으로 정렬하는 기능 구현

---
+++ src/main/java/wad/seoul_nolgoat/service/search/dto/DistanceSortCombinationDto.java
@@ -0,0 +1,39 @@
+package wad.seoul_nolgoat.service.search.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import wad.seoul_nolgoat.service.tMap.dto.WalkRouteInfoDto;
+
+@Getter
+public class DistanceSortCombinationDto {
+
+    private StoreForDistanceSortDto firstStore;
+    private StoreForDistanceSortDto secondStore;
+    private StoreForDistanceSortDto thirdStore;
+    private WalkRouteInfoDto walkRouteInfoDto;
+
+    @Setter
+    private double totalDistnace;
+
+    public DistanceSortCombinationDto(StoreForDistanceSortDto firstStore, StoreForDistanceSortDto secondStore, StoreForDistanceSortDto thirdStore, WalkRouteInfoDto walkRouteInfoDto) {
+        this.firstStore = firstStore;
+        this.secondStore = secondStore;
+        this.thirdStore = thirdStore;
+        this.walkRouteInfoDto = walkRouteInfoDto;
+    }
+
+    public DistanceSortCombinationDto(StoreForDistanceSortDto firstStore, StoreForDistanceSortDto secondStore, StoreForDistanceSortDto thirdStore) {
+        this.firstStore = firstStore;
+        this.secondStore = secondStore;
+        this.thirdStore = thirdStore;
+    }
+
+    public DistanceSortCombinationDto(StoreForDistanceSortDto firstStore, StoreForDistanceSortDto secondStore) {
+        this.firstStore = firstStore;
+        this.secondStore = secondStore;
+    }
+
+    public DistanceSortCombinationDto(StoreForDistanceSortDto firstStore) {
+        this.firstStore = firstStore;
+    }
+}
---
+++ src/main/java/wad/seoul_nolgoat/service/search/dto/StoreForDistanceSortDto.java
@@ -0,0 +1,17 @@
+package wad.seoul_nolgoat.service.search.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import wad.seoul_nolgoat.web.search.dto.CoordinateDto;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StoreForDistanceSortDto {
+
+    private Long id;
+    private String name;
+    private CoordinateDto coordinate;
+}
