bdc09ca7d38d4bd09cf3bd3caef158d2d38c7797
yonghyeonpark <pyh4616@naver.com>
2024-08-02 19:02:13 +0900

    feat: 차수에 따라 매개변수를 다르게 받을 수 있도록 메서드 오버로딩 구현

--- src/main/java/wad/seoul_nolgoat/web/search/dto/response/CombinationDto.java
+++ src/main/java/wad/seoul_nolgoat/web/search/dto/response/CombinationDto.java
@@ -1,14 +1,30 @@
 package wad.seoul_nolgoat.web.search.dto.response;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import wad.seoul_nolgoat.web.store.dto.response.StoreForCombinationDto;
 
 @Getter
-@RequiredArgsConstructor
 public class CombinationDto {
 
-    private final StoreForCombinationDto firstStore;
-    private final StoreForCombinationDto secondStore;
-    private final StoreForCombinationDto thirdStore;
+    private StoreForCombinationDto firstStore;
+    private StoreForCombinationDto secondStore;
+    private StoreForCombinationDto thirdStore;
+
+    public CombinationDto(
+            StoreForCombinationDto firstStore,
+            StoreForCombinationDto secondStore,
+            StoreForCombinationDto thirdStore) {
+        this.firstStore = firstStore;
+        this.secondStore = secondStore;
+        this.thirdStore = thirdStore;
+    }
+
+    public CombinationDto(StoreForCombinationDto firstStore, StoreForCombinationDto secondStore) {
+        this.firstStore = firstStore;
+        this.secondStore = secondStore;
+    }
+
+    public CombinationDto(StoreForCombinationDto firstStore) {
+        this.firstStore = firstStore;
+    }
 }
