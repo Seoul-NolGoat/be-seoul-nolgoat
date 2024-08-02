9476d1fd13930a8374034eba76b47a9eb230b9fd
yonghyeonpark <pyh4616@naver.com>
2024-08-02 19:04:13 +0900

    feat: CombinationDto 변경사항 반영

--- src/main/java/wad/seoul_nolgoat/util/mapper/CombinationMapper.java
+++ src/main/java/wad/seoul_nolgoat/util/mapper/CombinationMapper.java
@@ -1,5 +1,6 @@
 package wad.seoul_nolgoat.util.mapper;
 
+import wad.seoul_nolgoat.service.search.SearchService;
 import wad.seoul_nolgoat.service.search.dto.DistanceSortCombinationDto;
 import wad.seoul_nolgoat.service.search.dto.GradeSortCombinationDto;
 import wad.seoul_nolgoat.web.search.dto.response.CombinationDto;
@@ -7,18 +8,46 @@ import wad.seoul_nolgoat.web.search.dto.response.CombinationDto;
 public class CombinationMapper {
 
     public static CombinationDto toCombinationDto(DistanceSortCombinationDto distanceSortCombinationDto) {
-        return new CombinationDto(
-                StoreMapper.toStoreForCombinationDto(distanceSortCombinationDto.getFirstStore()),
-                StoreMapper.toStoreForCombinationDto(distanceSortCombinationDto.getSecondStore()),
-                StoreMapper.toStoreForCombinationDto(distanceSortCombinationDto.getThirdStore())
-        );
+        if (distanceSortCombinationDto.getTotalRounds() == SearchService.THREE_ROUND) {
+            return new CombinationDto(
+                    StoreMapper.toStoreForCombinationDto(distanceSortCombinationDto.getFirstStore()),
+                    StoreMapper.toStoreForCombinationDto(distanceSortCombinationDto.getSecondStore()),
+                    StoreMapper.toStoreForCombinationDto(distanceSortCombinationDto.getThirdStore())
+            );
+        }
+        if (distanceSortCombinationDto.getTotalRounds() == SearchService.TWO_ROUND) {
+            return new CombinationDto(
+                    StoreMapper.toStoreForCombinationDto(distanceSortCombinationDto.getFirstStore()),
+                    StoreMapper.toStoreForCombinationDto(distanceSortCombinationDto.getSecondStore())
+            );
+        }
+        if (distanceSortCombinationDto.getTotalRounds() == SearchService.ONE_ROUND) {
+            return new CombinationDto(
+                    StoreMapper.toStoreForCombinationDto(distanceSortCombinationDto.getFirstStore())
+            );
+        }
+        throw new RuntimeException();
     }
 
     public static CombinationDto toCombinationDto(GradeSortCombinationDto gradeSortCombinationDto) {
-        return new CombinationDto(
-                StoreMapper.toStoreForCombinationDto(gradeSortCombinationDto.getFirstStore()),
-                StoreMapper.toStoreForCombinationDto(gradeSortCombinationDto.getSecondStore()),
-                StoreMapper.toStoreForCombinationDto(gradeSortCombinationDto.getThirdStore())
-        );
+        if (gradeSortCombinationDto.getTotalRounds() == SearchService.THREE_ROUND) {
+            return new CombinationDto(
+                    StoreMapper.toStoreForCombinationDto(gradeSortCombinationDto.getFirstStore()),
+                    StoreMapper.toStoreForCombinationDto(gradeSortCombinationDto.getSecondStore()),
+                    StoreMapper.toStoreForCombinationDto(gradeSortCombinationDto.getThirdStore())
+            );
+        }
+        if (gradeSortCombinationDto.getTotalRounds() == SearchService.TWO_ROUND) {
+            return new CombinationDto(
+                    StoreMapper.toStoreForCombinationDto(gradeSortCombinationDto.getFirstStore()),
+                    StoreMapper.toStoreForCombinationDto(gradeSortCombinationDto.getSecondStore())
+            );
+        }
+        if (gradeSortCombinationDto.getTotalRounds() == SearchService.ONE_ROUND) {
+            return new CombinationDto(
+                    StoreMapper.toStoreForCombinationDto(gradeSortCombinationDto.getFirstStore())
+            );
+        }
+        throw new RuntimeException();
     }
 }
