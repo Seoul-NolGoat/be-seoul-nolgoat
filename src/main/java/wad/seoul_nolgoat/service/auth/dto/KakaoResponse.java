6c13a8e36a922afcac7bfb4d43cfcbd1216044b6
yonghyeonpark <pyh4616@naver.com>
2024-08-18 18:58:55 +0900

    feat: OAuth2Response 인터페이스를 구현한 KakaoResponse 추가

---
+++ src/main/java/wad/seoul_nolgoat/service/auth/dto/KakaoResponse.java
@@ -0,0 +1,35 @@
+package wad.seoul_nolgoat.service.auth.dto;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class KakaoResponse implements OAuth2Response {
+
+    private final Map<String, Object> attributes;
+
+    @Override
+    public String getProvider() {
+        return "kakao";
+    }
+
+    @Override
+    public String getProviderId() {
+        return attributes.get("id").toString();
+    }
+
+    @Override
+    public String getNickname() {
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        Map<String, String> profile = (Map<String, String>) kakaoAccount.get("profile");
+        return profile.get("nickname");
+    }
+
+    @Override
+    public String getProfileImage() {
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        Map<String, String> profile = (Map<String, String>) kakaoAccount.get("profile");
+        return profile.get("profile_image_url");
+    }
+}
