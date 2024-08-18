c053319d4e355404760415e025e0a9c3e941263c
yonghyeonpark <pyh4616@naver.com>
2024-08-18 23:26:11 +0900

    feat: 스프링 시큐리티 설정 파일 구현
    - 일반 로그인에서 비밀번호 암호화에 사용하기 위해 BCryptPasswordEncoder를 미리 빈으로 등록
    - OAuth2 로그인과 JWT 필터 설정
    - 특정 API에 인증 설정

---
+++ src/main/java/wad/seoul_nolgoat/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package wad.seoul_nolgoat.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import wad.seoul_nolgoat.jwt.JwtFilter;
+import wad.seoul_nolgoat.jwt.JwtUtil;
+import wad.seoul_nolgoat.service.auth.CustomOAuth2UserService;
+import wad.seoul_nolgoat.service.auth.CustomSuccessHandler;
+
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomSuccessHandler customSuccessHandler;
+    private final JwtUtil jwtUtil;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService)
+                        )
+                        .successHandler(customSuccessHandler)
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/reviews/**", "/api/search/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(s -> s
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+        return http.build();
+    }
+}
