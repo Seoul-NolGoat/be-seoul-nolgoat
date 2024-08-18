94b1c0c30d1bc3cd2d9772d729481424c3dbed96
yonghyeonpark <pyh4616@naver.com>
2024-08-18 18:49:58 +0900

    feat: JWT 생성 및 정보 추출 메서드 구현

---
+++ src/main/java/wad/seoul_nolgoat/jwt/JwtUtil.java
@@ -0,0 +1,77 @@
+package wad.seoul_nolgoat.jwt;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final SecretKey secretKey;
+
+    public JwtUtil(@Value("${spring.jwt.secret}") String secret) {
+        this.secretKey = new SecretKeySpec(
+                secret.getBytes(StandardCharsets.UTF_8),
+                Jwts.SIG.HS256.key().build().getAlgorithm()
+        );
+    }
+
+    //
+    public String getLoginId(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("loginId", String.class);
+    }
+
+    public String getNickname(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("nickname", String.class);
+    }
+
+    public String getProfileImage(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("profileImage", String.class);
+    }
+
+    public boolean isExpired(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration()
+                .before(new Date());
+    }
+
+    public String createJwt(
+            String loginId,
+            String nickname,
+            String profileImage,
+            Long expiredMs
+    ) {
+        return Jwts.builder()
+                .claim("loginId", loginId)
+                .claim("nickname", nickname)
+                .claim("profileImage", profileImage)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(secretKey)
+                .compact();
+    }
+}
