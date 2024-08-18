f2db05c835be1b1d10743d61b33364d02596c680
yonghyeonpark <pyh4616@naver.com>
2024-08-18 23:11:50 +0900

    feat: OAuth2 로그인 성공 시, 토큰을 반환해주는 로직 구현

---
+++ src/main/java/wad/seoul_nolgoat/service/auth/CustomSuccessHandler.java
@@ -0,0 +1,35 @@
+package wad.seoul_nolgoat.service.auth;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import wad.seoul_nolgoat.jwt.JwtUtil;
+import wad.seoul_nolgoat.service.auth.dto.OAuth2UserImpl;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Component
+public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private static final String REDIRECT_URL = "http://localhost:3000/loginSuccess?token=";
+    private static final Long JWT_EXPIRATION_TIME = 30 * 60 * 1000L; // 30 minute
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        OAuth2UserImpl oAuth2User = (OAuth2UserImpl) authentication.getPrincipal();
+        String jwt = jwtUtil.createJwt(
+                oAuth2User.getName(),
+                oAuth2User.getNickname(),
+                oAuth2User.getProfileImage(),
+                JWT_EXPIRATION_TIME
+        );
+        response.sendRedirect(REDIRECT_URL + jwt);
+    }
+}
