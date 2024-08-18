02bb2d9eadd3b1cc1ebf18c362b2157a9ed5e61c
yonghyeonpark <pyh4616@naver.com>
2024-08-18 18:53:38 +0900

    feat: User에 대해 existsByLoginId()와 findByLoginId() 메서드 구현

--- src/main/java/wad/seoul_nolgoat/domain/user/UserRepository.java
+++ src/main/java/wad/seoul_nolgoat/domain/user/UserRepository.java
@@ -2,5 +2,11 @@ package wad.seoul_nolgoat.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByLoginId(String LoginId);
+
+    Optional<User> findByLoginId(String LoginId);
 }
