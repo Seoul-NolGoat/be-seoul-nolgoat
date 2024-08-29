2569a456371d5f6e669e7f36f127babd6f87621f
yonghyeonpark <pyh4616@naver.com>
2024-08-29 23:22:14 +0900

    feat: Refresh, Access 토큰을 재발급해주는 API 구현

--- src/main/java/wad/seoul_nolgoat/web/auth/AuthController.java
+++ src/main/java/wad/seoul_nolgoat/web/auth/AuthController.java
@@ -1,9 +1,12 @@
 package wad.seoul_nolgoat.web.auth;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import wad.seoul_nolgoat.service.auth.AuthService;
@@ -17,13 +20,37 @@ public class AuthController {
     private final AuthService authService;
 
     @GetMapping("/user_profile")
-    public ResponseEntity<UserProfileDto> showUserProfile(@RequestHeader(value = "Authorization", required = false) String authorization) {
+    public ResponseEntity<UserProfileDto> showUserProfile(HttpServletRequest request) {
+        String authorization = request.getHeader(AuthService.AUTHORIZATION_HEADER_TYPE);
         if (authorization == null || authorization.isBlank()) {
             return ResponseEntity
                     .noContent()
                     .build();
         }
+
         return ResponseEntity
                 .ok(authService.findLoginUserByAuthorization(authorization));
+    }
+
+    @PostMapping("/reissue")
+    public ResponseEntity reissueTokens(HttpServletRequest request, HttpServletResponse response) {
+        String refreshToken = request.getHeader(AuthService.REFRESH_HEADER_TYPE);
+        if (authService.isInvalidRefreshToken(refreshToken)
+                || authService.isExpiredToken(refreshToken)
+                || !authService.isExistRefreshToken(refreshToken)) {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .build();
+        }
+        authService.deleteRefreshToken(refreshToken);
+        String newRefreshToken = authService.getNewRefreshToken(refreshToken);
+        authService.saveRefreshToken(newRefreshToken);
+
+        response.setHeader(AuthService.AUTHORIZATION_HEADER_TYPE, authService.getNewAccessToken(refreshToken));
+        response.setHeader(AuthService.REFRESH_HEADER_TYPE, newRefreshToken);
+
+        return ResponseEntity
+                .ok()
+                .build();
     }
 }
