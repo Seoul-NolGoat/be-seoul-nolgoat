39fc1a14ce837cbff245fff300ed55589ce0d8bc
yonghyeonpark <pyh4616@naver.com>
2024-09-03 00:54:47 +0900

    feat: 예외 메시지 관리를 위한 클래스 구현

---
+++ src/main/java/wad/seoul_nolgoat/exception/ErrorMessages.java
@@ -0,0 +1,11 @@
+package wad.seoul_nolgoat.exception;
+
+public class ErrorMessages {
+
+    public static final String USER_NOT_FOUND_MESSAGE = "존재하지 않는 유저입니다.";
+    public static final String STORE_NOT_FOUND_MESSAGE = "존재하지 않는 가게입니다.";
+    public static final String REVIEW_NOT_FOUND_MESSAGE = "존재하지 않는 리뷰입니다.";
+
+    private ErrorMessages() {
+    }
+}
