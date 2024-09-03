951632930c0d871e5a82a47e397ce57b14a790f1
yonghyeonpark <pyh4616@naver.com>
2024-09-03 23:15:25 +0900

    feat: 글로벌 예외 처리 핸들러 구현

---
+++ src/main/java/wad/seoul_nolgoat/web/exception/GlobalExceptionHandler.java
@@ -0,0 +1,46 @@
+package wad.seoul_nolgoat.web.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import wad.seoul_nolgoat.exception.*;
+import wad.seoul_nolgoat.web.exception.dto.response.ErrorResult;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> handleUserNotFoundException(UserNotFoundException e) {
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResult(e.getMessage()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> handleStoreNotFoundException(StoreNotFoundException e) {
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResult(e.getMessage()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> handleReviewNotFoundException(ReviewNotFoundException e) {
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResult(e.getMessage()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> handleInvalidSearchCriteriaException(InvalidSearchCriteriaException e) {
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResult(e.getMessage()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResult> handleInvalidRoundException(InvalidRoundException e) {
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResult(e.getMessage()));
+    }
+}
