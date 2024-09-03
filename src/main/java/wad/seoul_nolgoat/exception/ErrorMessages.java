de4d76c4e8fa417b4cf5c2755511e1e102e89510
yonghyeonpark <pyh4616@naver.com>
2024-09-03 23:10:58 +0900

    feat: 예외 발생 메시지를 관리하기 위한 클래스 구현

--- src/main/java/wad/seoul_nolgoat/exception/ErrorMessages.java
+++ src/main/java/wad/seoul_nolgoat/exception/ErrorMessages.java
@@ -5,6 +5,8 @@ public class ErrorMessages {
     public static final String USER_NOT_FOUND_MESSAGE = "존재하지 않는 유저입니다.";
     public static final String STORE_NOT_FOUND_MESSAGE = "존재하지 않는 가게입니다.";
     public static final String REVIEW_NOT_FOUND_MESSAGE = "존재하지 않는 리뷰입니다.";
+    public static final String INVALID_GATHERING_ROUND = "유효하지 않은 회식 차수입니다.";
+    public static final String INVALID_SEARCH_CRITERIA = "유효하지 않은 검색 기준입니다.";
 
     private ErrorMessages() {
     }
