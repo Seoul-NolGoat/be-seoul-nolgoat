798c702d04e4027773b2bce47074a578369694e8
yonghyeonpark <pyh4616@naver.com>
2024-09-03 22:40:01 +0900

    feat: 잘못된 검색 기준, 회식 차수에 대한 커스텀 예외 구현

---
+++ src/main/java/wad/seoul_nolgoat/exception/InvalidRoundException.java
@@ -0,0 +1,8 @@
+package wad.seoul_nolgoat.exception;
+
+public class InvalidRoundException extends RuntimeException {
+
+    public InvalidRoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/wad/seoul_nolgoat/exception/InvalidSearchCriteriaException.java
@@ -0,0 +1,8 @@
+package wad.seoul_nolgoat.exception;
+
+public class InvalidSearchCriteriaException extends RuntimeException {
+
+    public InvalidSearchCriteriaException(String message) {
+        super(message);
+    }
+}
