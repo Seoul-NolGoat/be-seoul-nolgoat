aa87b1f693977215b44f4a83def55754b54eb669
Daniel <923daniel@naver.com>
2024-09-04 22:33:19 +0900

    feat: AWS S3에 저장된 이미지 파일을 삭제하는 기능 구현

--- src/main/java/wad/seoul_nolgoat/service/s3/S3Service.java
+++ src/main/java/wad/seoul_nolgoat/service/s3/S3Service.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -29,6 +32,19 @@ public class S3Service {
 
         amazonS3.putObject(bucket, filename, multipartFile.getInputStream(), metadata);
         return amazonS3.getUrl(bucket, filename).toString();
+    }
+
+    public void deleteFile(String fileUrl) {
+        try {
+            URL url = new URL(fileUrl);
+            String path = url.getPath();
+            String key = path.substring(1);
+            String decodedKey = URLDecoder.decode(key, StandardCharsets.UTF_8); // 한글 또는 특수문자가 있는 경우를 위해
+
+            amazonS3.deleteObject(bucket, decodedKey);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to delete file from S3: " + fileUrl, e);
+        }
     }
 
     private String generateFileName(String originalFilename) {
