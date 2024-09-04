253045d8dce48b8b1cba3d6f2de4f265272fb978
Daniel <923daniel@naver.com>
2024-09-05 08:28:52 +0900

    feat: ReviewController에서 Review를 생성할 때 MultipartFile을 받을 수 있도록 구현

--- src/main/java/wad/seoul_nolgoat/web/review/ReviewController.java
+++ src/main/java/wad/seoul_nolgoat/web/review/ReviewController.java
@@ -3,14 +3,18 @@ package wad.seoul_nolgoat.web.review;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
 import wad.seoul_nolgoat.service.review.ReviewService;
+import wad.seoul_nolgoat.service.s3.S3Service;
 import wad.seoul_nolgoat.web.review.dto.request.ReviewSaveDto;
 import wad.seoul_nolgoat.web.review.dto.request.ReviewUpdateDto;
 import wad.seoul_nolgoat.web.review.dto.response.ReviewDetailsForUserDto;
 
+import java.io.IOException;
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/reviews")
@@ -18,16 +22,28 @@ import java.util.List;
 public class ReviewController {
 
     private final ReviewService reviewService;
+    private final S3Service s3Service;
 
     @PostMapping("/{userId}/{storeId}")
     public ResponseEntity<Void> createReview(
             @PathVariable Long userId,
             @PathVariable Long storeId,
-            @RequestBody ReviewSaveDto reviewSaveDto,
+            @RequestPart(value = "file", required = false) MultipartFile file,
+            @RequestPart("review") ReviewSaveDto reviewSaveDto,
             UriComponentsBuilder uriComponentsBuilder) {
+        Optional<String> optionalImageUrl = Optional.empty();
+        if (file != null && !file.isEmpty()) {
+            try {
+                optionalImageUrl = Optional.of(s3Service.saveFile(file));
+            } catch (IOException e) {
+                throw new RuntimeException();
+            }
+        }
+
         Long reviewId = reviewService.save(
                 userId,
                 storeId,
+                optionalImageUrl,
                 reviewSaveDto
         );
         URI location = uriComponentsBuilder.path("/api/reviews/{reviewId}")
@@ -45,6 +61,7 @@ public class ReviewController {
                 .ok(reviewService.findByUserId(userId));
     }
 
+    // 현재 사용하지 않음
     @PutMapping("/{reviewId}")
     public ResponseEntity<Void> update(@PathVariable Long reviewId, @RequestBody ReviewUpdateDto reviewUpdateDto) {
         reviewService.update(reviewId, reviewUpdateDto);
