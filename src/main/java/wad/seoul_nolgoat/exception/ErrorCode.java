139a998c9ee19facb378c87d3930837425c1d29c
Daniel <923daniel@naver.com>
2024-09-08 09:42:06 +0900

    feat: 상태코드와 에러 메세지를 저장하는 ErrorCode enum 클래스 구현

---
+++ src/main/java/wad/seoul_nolgoat/exception/ErrorCode.java
@@ -0,0 +1,24 @@
+package wad.seoul_nolgoat.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 유저입니다."),
+    STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 가게입니다."),
+    REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 리뷰입니다."),
+    INVALID_GATHERING_ROUND(HttpStatus.BAD_REQUEST, "유효하지 않은 회식 차수입니다."),
+    INVALID_SEARCH_CRITERIA(HttpStatus.BAD_REQUEST, "유효하지 않은 검색 기준입니다."),
+    TOKEN_EXPIRED_MESSAGE(HttpStatus.UNAUTHORIZED, "기한이 만료된 토큰입니다."),
+    UNSUPPORTED_PROVIDER(HttpStatus.UNAUTHORIZED, "지원하지 않는 제공자입니다."),
+    ADDRESS_CONVERSION_FAILED(HttpStatus.BAD_REQUEST, "주소로 변환할 수 없습니다."),
+    WALKING_DISTANCE_CALCULATION_FAILED(HttpStatus.BAD_REQUEST, "도보 거리를 계산할 수 없습니다."),
+    TMAP_API_CALL_FAILED(HttpStatus.BAD_REQUEST, "TMap API 호출에 실패했습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
