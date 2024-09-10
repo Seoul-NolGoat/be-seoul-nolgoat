79d54985d9a0a955e4fe946a58edda41b431c74e
Daniel <923daniel@naver.com>
2024-09-11 08:12:00 +0900

    feat: 주어진 userId와 storeId를 포함하는 Bookmark가 있는지 확인하는 쿼리 메서드 구현

--- src/main/java/wad/seoul_nolgoat/domain/bookmark/BookmarkRepository.java
+++ src/main/java/wad/seoul_nolgoat/domain/bookmark/BookmarkRepository.java
@@ -3,4 +3,6 @@ package wad.seoul_nolgoat.domain.bookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
+    boolean existsByUserIdAndStoreId(Long userId, Long storeId);
 }
