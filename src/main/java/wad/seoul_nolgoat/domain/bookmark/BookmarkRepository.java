329dea7a0591cc48eba49e3e5741bec59245f964
Daniel <923daniel@naver.com>
2024-09-11 08:18:49 +0900

    feat: 주어진 userId와 storeId를 포함하는 Bookmark를 삭제하는 쿼리 메서드 구현

--- src/main/java/wad/seoul_nolgoat/domain/bookmark/BookmarkRepository.java
+++ src/main/java/wad/seoul_nolgoat/domain/bookmark/BookmarkRepository.java
@@ -1,8 +1,17 @@
 package wad.seoul_nolgoat.domain.bookmark;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
     boolean existsByUserIdAndStoreId(Long userId, Long storeId);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Bookmark b WHERE b.user.id = :userId AND b.store.id = :storeId")
+    void deleteByUserIdAndStoreId(@Param("userId") Long userId, @Param("storeId") Long storeId);
 }
