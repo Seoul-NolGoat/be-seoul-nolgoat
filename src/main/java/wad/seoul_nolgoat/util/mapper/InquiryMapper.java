4b7e8f5b21ce3e4f1857051fffecfc17142c4a50
Daniel <923daniel@naver.com>
2024-09-17 15:59:48 +0900

    feat: Inquiry 엔터티, Dto의 상호 변환을 위한 Mapper 클래스 구현

---
+++ src/main/java/wad/seoul_nolgoat/util/mapper/InquiryMapper.java
@@ -0,0 +1,33 @@
+package wad.seoul_nolgoat.util.mapper;
+
+import wad.seoul_nolgoat.domain.inquiry.Inquiry;
+import wad.seoul_nolgoat.domain.user.User;
+import wad.seoul_nolgoat.web.inquiry.dto.request.InquirySaveDto;
+import wad.seoul_nolgoat.web.inquiry.dto.response.InquiryDetailsDto;
+
+import static wad.seoul_nolgoat.util.DateTimeUtil.formatDate;
+
+public class InquiryMapper {
+
+    public static Inquiry toEntity(User user, InquirySaveDto inquirySaveDto) {
+        return new Inquiry(
+                inquirySaveDto.getTitle(),
+                inquirySaveDto.getContent(),
+                inquirySaveDto.getIsPublic(),
+                user
+        );
+    }
+
+    public static InquiryDetailsDto toInquiryDetailsDto(Inquiry inquiry) {
+        return new InquiryDetailsDto(
+                inquiry.getId(),
+                inquiry.getTitle(),
+                inquiry.getContent(),
+                inquiry.getIsPublic(),
+                inquiry.getUser().getId(),
+                inquiry.getUser().getNickname(),
+                inquiry.getUser().getProfileImage(),
+                formatDate(inquiry.getCreatedDate())
+        );
+    }
+}
