d87f7f336387d9196ef6932522c420bfce88ba51
Daniel <923daniel@naver.com>
2024-09-25 10:12:33 +0900

    feat: Notice CRUD API 구현

---
+++ src/main/java/wad/seoul_nolgoat/web/notice/NoticeController.java
@@ -0,0 +1,79 @@
+package wad.seoul_nolgoat.web.notice;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+import wad.seoul_nolgoat.service.notice.NoticeService;
+import wad.seoul_nolgoat.util.ValidationUtil;
+import wad.seoul_nolgoat.web.notice.dto.request.NoticeSaveDto;
+import wad.seoul_nolgoat.web.notice.dto.request.NoticeUpdateDto;
+import wad.seoul_nolgoat.web.notice.dto.response.NoticeDetailsDto;
+import wad.seoul_nolgoat.web.notice.dto.response.NoticeListDto;
+
+import java.net.URI;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/notices")
+@RestController
+public class NoticeController {
+
+    private final NoticeService noticeService;
+
+    @PostMapping("/{userId}")
+    public ResponseEntity<Void> createNotice(
+            @PathVariable Long userId,
+            @RequestBody NoticeSaveDto noticeSaveDto,
+            UriComponentsBuilder uriComponentsBuilder
+    ) {
+        Long noticeId = noticeService.save(userId, noticeSaveDto);
+        URI location = uriComponentsBuilder.path("/api/notices/{noticeId}")
+                .buildAndExpand(noticeId)
+                .toUri();
+
+        return ResponseEntity
+                .created(location)
+                .build();
+    }
+
+    @GetMapping("/{noticeId}")
+    public ResponseEntity<NoticeDetailsDto> showNoticeByNoticeId(@PathVariable Long noticeId) {
+        return ResponseEntity
+                .ok(noticeService.findByNoticeId(noticeId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<NoticeListDto>> showAllNotices() {
+        return ResponseEntity
+                .ok(noticeService.findAllNotice());
+    }
+
+    @PutMapping("/{noticeId}")
+    public ResponseEntity<?> update(
+            @PathVariable Long noticeId,
+            @RequestBody NoticeUpdateDto noticeUpdateDto,
+            BindingResult bindingResult
+    ) {
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(ValidationUtil.extractErrors(bindingResult));
+        }
+        noticeService.update(noticeId, noticeUpdateDto);
+
+        return ResponseEntity
+                .noContent()
+                .build();
+    }
+
+    @DeleteMapping("/{noticeId}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long noticeId) {
+        noticeService.deleteById(noticeId);
+
+        return ResponseEntity
+                .noContent()
+                .build();
+    }
+}
