8efebdf25a1572cf2919998f9a65c4c3e1237860
yonghyeonpark <pyh4616@naver.com>
2024-10-09 01:43:15 +0900

    feat: MethodArgumentNotValidException 처리 추가

--- src/main/java/wad/seoul_nolgoat/exception/GlobalExceptionHandler.java
+++ src/main/java/wad/seoul_nolgoat/exception/GlobalExceptionHandler.java
@@ -1,18 +1,40 @@
 package wad.seoul_nolgoat.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import wad.seoul_nolgoat.web.exception.dto.response.ErrorResponse;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler
+    public GlobalExceptionHandler() {
+    }
+
+    @ExceptionHandler(ApiException.class)
     public ResponseEntity<ErrorResponse> handleApiException(ApiException e) {
         ErrorCode errorCode = e.getErrorCode();
+
         return ResponseEntity
                 .status(errorCode.getHttpStatus())
                 .body(new ErrorResponse(errorCode));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        ErrorCode errorCode = ErrorCode.INVALID_INPUT_VALUE;
+
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(new ErrorResponse(errorCode, errors));
     }
 }
