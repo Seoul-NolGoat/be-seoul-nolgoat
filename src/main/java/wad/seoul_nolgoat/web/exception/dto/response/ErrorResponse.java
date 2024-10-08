d03ca0089fa917b01148e1f6a6267afe5f15c097
yonghyeonpark <pyh4616@naver.com>
2024-10-09 00:49:26 +0900

    refactor: 에러 응답 Dto 이름 수정 및 필드 추가

--- src/main/java/wad/seoul_nolgoat/web/exception/dto/response/ErrorResponse.java
+++ src/main/java/wad/seoul_nolgoat/web/exception/dto/response/ErrorResponse.java
@@ -2,10 +2,13 @@ package wad.seoul_nolgoat.web.exception.dto.response;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
-public class ErrorResult {
+public class ErrorResponse {
 
+    private final HttpStatus status;
+    private final String code;
     private final String message;
 }
