f7be4c9d733b1efe1ba144bb5b845cad9114b122
yonghyeonpark <pyh4616@naver.com>
2024-10-21 19:58:44 +0900

    feat: 인증 관련해서 요청 URI를 관리하는 클래스 구현

---
+++ src/main/java/wad/seoul_nolgoat/auth/AuthUrlManager.java
@@ -0,0 +1,31 @@
+package wad.seoul_nolgoat.auth;
+
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+public class AuthUrlManager {
+
+    public static RequestMatcher[] getUserRequestMatchers() {
+        return new RequestMatcher[]{
+                new AntPathRequestMatcher("/api/auths/me"),
+                new AntPathRequestMatcher("/api/auths/logout"),
+
+                new AntPathRequestMatcher("/api/stores/**"),
+
+                new AntPathRequestMatcher("/api/reviews/**"),
+
+                new AntPathRequestMatcher("/api/bookmarks/**"),
+
+                new AntPathRequestMatcher("/api/search/**"),
+
+                new AntPathRequestMatcher("/api/inquiries/{userId}", "POST"),
+                new AntPathRequestMatcher("/api/inquiries/{inquiryId}", "PUT"),
+                new AntPathRequestMatcher("/api/inquiries/{inquiryId}", "DELETE"),
+
+                new AntPathRequestMatcher("/api/notices/{userId}", "POST"),
+                new AntPathRequestMatcher("/api/notices/{noticeId}", "PUT"),
+                new AntPathRequestMatcher("/api/notices/{noticeId}/views", "PUT"),
+                new AntPathRequestMatcher("/api/notices/{noticeId}", "DELETE")
+        };
+    }
+}
