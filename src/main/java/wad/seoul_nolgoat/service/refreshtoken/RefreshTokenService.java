154cbc74c1e363e643adebe9073862e46a05e168
yonghyeonpark <pyh4616@naver.com>
2024-10-23 23:25:29 +0900

    feat: RefreshTokenRepository 관련 로직을 분리하여 Service 클래스 구현

---
+++ src/main/java/wad/seoul_nolgoat/service/refreshtoken/RefreshTokenService.java
@@ -0,0 +1,42 @@
+package wad.seoul_nolgoat.service.refreshtoken;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import wad.seoul_nolgoat.auth.jwt.JwtService;
+import wad.seoul_nolgoat.domain.refresh.RefreshToken;
+import wad.seoul_nolgoat.domain.refresh.RefreshTokenRepository;
+import wad.seoul_nolgoat.exception.ApiException;
+
+import java.util.Date;
+
+import static wad.seoul_nolgoat.exception.ErrorCode.REFRESH_TOKEN_NOT_FOUND;
+
+@RequiredArgsConstructor
+@Service
+public class RefreshTokenService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public void saveRefreshToken(String refreshToken, String loginId) {
+        Date date = new Date(System.currentTimeMillis() + JwtService.REFRESH_TOKEN_EXPIRATION_TIME);
+        refreshTokenRepository.save(
+                new RefreshToken(
+                        refreshToken,
+                        loginId,
+                        date.toString()
+                )
+        );
+    }
+
+    @Transactional
+    public void deleteRefreshToken(String refreshToken) {
+        refreshTokenRepository.deleteByRefreshToken(refreshToken);
+    }
+
+    public void verifyRefreshTokenExistence(String refreshToken) {
+        if (!refreshTokenRepository.existsByRefreshToken(refreshToken)) {
+            throw new ApiException(REFRESH_TOKEN_NOT_FOUND);
+        }
+    }
+}
