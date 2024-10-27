bf6863b1f84a19b491a975ca7cecb57a97f59cdb
yonghyeonpark <pyh4616@naver.com>
2024-10-28 02:29:03 +0900

    feat: 파티 생성 API 구현

---
+++ src/main/java/wad/seoul_nolgoat/web/party/PartyController.java
@@ -0,0 +1,47 @@
+package wad.seoul_nolgoat.web.party;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.util.UriComponentsBuilder;
+import wad.seoul_nolgoat.service.party.PartyService;
+import wad.seoul_nolgoat.web.party.request.PartySaveDto;
+
+import java.net.URI;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/parties/")
+@RestController
+public class PartyController {
+
+    private final PartyService partyService;
+
+    @PostMapping
+    public ResponseEntity<Void> createParty(
+            @AuthenticationPrincipal OAuth2User loginUser,
+            @RequestPart("party") PartySaveDto partySaveDto,
+            @RequestPart(value = "file", required = false) MultipartFile file,
+            UriComponentsBuilder uriComponentsBuilder
+    ) {
+        Long partyId = partyService.createParty(
+                loginUser.getName(),
+                partySaveDto,
+                Optional.ofNullable(file)
+        );
+
+        URI location = uriComponentsBuilder.path("/api/parties/{partyId}")
+                .buildAndExpand(partyId)
+                .toUri();
+
+        return ResponseEntity
+                .created(location)
+                .build();
+    }
+}
