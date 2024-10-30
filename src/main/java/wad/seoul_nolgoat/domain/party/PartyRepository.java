da43b04922b91a216341bb1bef8d08ed88c23c5e
yonghyeonpark <pyh4616@naver.com>
2024-10-31 04:18:03 +0900

    feat: 비관적 락을 사용하여 파티 객체를 조회하는 기능 구현

--- src/main/java/wad/seoul_nolgoat/domain/party/PartyRepository.java
+++ src/main/java/wad/seoul_nolgoat/domain/party/PartyRepository.java
@@ -1,6 +1,15 @@
 package wad.seoul_nolgoat.domain.party;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface PartyRepository extends JpaRepository<Party, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT p FROM Party p WHERE p.id = :id")
+    Optional<Party> findByIdWithLock(Long id);
 }
