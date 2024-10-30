7db2d6b94b0decaccaf5428f51ca3cb30e16091a
yonghyeonpark <pyh4616@naver.com>
2024-10-30 17:16:32 +0900

    refactor: 파라미터 인자로 옵셔널을 받는 대신 값을 직접 받도록 수정

--- src/main/java/wad/seoul_nolgoat/util/mapper/PartyMapper.java
+++ src/main/java/wad/seoul_nolgoat/util/mapper/PartyMapper.java
@@ -4,27 +4,16 @@ import wad.seoul_nolgoat.domain.party.Party;
 import wad.seoul_nolgoat.domain.user.User;
 import wad.seoul_nolgoat.web.party.request.PartySaveDto;
 
-import java.util.Optional;
-
 public class PartyMapper {
 
     public static Party toEntity(
             PartySaveDto partySaveDto,
             User user,
-            Optional<String> optionalImageUrl
+            String imageUrl
     ) {
-        if (optionalImageUrl.isPresent()) {
-            return new Party(
-                    partySaveDto.getTitle(),
-                    optionalImageUrl.get(),
-                    partySaveDto.getMaxCapacity(),
-                    partySaveDto.getDeadline(),
-                    user
-
-            );
-        }
         return new Party(
                 partySaveDto.getTitle(),
+                imageUrl,
                 partySaveDto.getMaxCapacity(),
                 partySaveDto.getDeadline(),
                 user
