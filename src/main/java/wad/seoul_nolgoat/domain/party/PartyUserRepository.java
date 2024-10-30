64fd241f065686d609d227077457906ee0a5ba5c
yonghyeonpark <pyh4616@naver.com>
2024-10-31 04:19:08 +0900

    feat: 파티에 참여한 유저수를 구하는 기능 구현

--- src/main/java/wad/seoul_nolgoat/domain/party/PartyUserRepository.java
+++ src/main/java/wad/seoul_nolgoat/domain/party/PartyUserRepository.java
@@ -1,6 +1,10 @@
 package wad.seoul_nolgoat.domain.party;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PartyUserRepository extends JpaRepository<PartyUser, Long> {
+
+    @Query("SELECT COUNT(pu) FROM PartyUser pu WHERE pu.party.id = :partyId")
+    int countByPartyId(Long partyId);
 }
