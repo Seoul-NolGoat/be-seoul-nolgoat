aa0acef8b52f292c26e6d0d5059aca952a87c79c
yonghyeonpark <pyh4616@naver.com>
2024-11-02 19:43:28 +0900

    feat: 파티 참여 API 구현

--- src/main/java/wad/seoul_nolgoat/web/party/PartyController.java
+++ src/main/java/wad/seoul_nolgoat/web/party/PartyController.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
 import wad.seoul_nolgoat.service.party.PartyService;
@@ -41,6 +38,14 @@ public class PartyController {
 
         return ResponseEntity
                 .created(location)
+                .build();
+    }
+
+    @PostMapping("/{partyId}/join")
+    public ResponseEntity<Void> joinParty(@AuthenticationPrincipal OAuth2User loginUser, @PathVariable Long partyId) {
+        partyService.joinParty(loginUser.getName(), partyId);
+        return ResponseEntity
+                .ok()
                 .build();
     }
 }
