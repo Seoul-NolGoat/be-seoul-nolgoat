3ec4edcf12bc55f1e616b7dd90014ab48346d33c
yonghyeonpark <pyh4616@naver.com>
2024-11-03 02:06:07 +0900

    feat: partyId와 participantId로 PartyUser 객체를 찾는 기능 구현

--- src/main/java/wad/seoul_nolgoat/domain/party/PartyUserRepository.java
+++ src/main/java/wad/seoul_nolgoat/domain/party/PartyUserRepository.java
@@ -3,10 +3,14 @@ package wad.seoul_nolgoat.domain.party;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface PartyUserRepository extends JpaRepository<PartyUser, Long> {
 
     @Query("SELECT COUNT(pu) FROM PartyUser pu WHERE pu.party.id = :partyId")
     int countByPartyId(Long partyId);
 
     boolean existsByPartyIdAndParticipantId(Long partyId, Long participantId);
+
+    Optional<PartyUser> findByPartyIdAndParticipantId(Long partyId, Long participantId);
 }
