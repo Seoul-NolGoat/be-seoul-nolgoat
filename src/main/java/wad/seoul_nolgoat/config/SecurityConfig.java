c239a9d20affe57f54f5a00495b4ac6658691e14
yonghyeonpark <pyh4616@naver.com>
2024-11-23 05:57:27 +0900

    fix: SecurityConfig에 누락된 어노테이션 추가

--- src/main/java/wad/seoul_nolgoat/config/SecurityConfig.java
+++ src/main/java/wad/seoul_nolgoat/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,6 +25,7 @@ import wad.seoul_nolgoat.auth.service.AuthService;
 import java.util.List;
 
 @RequiredArgsConstructor
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
