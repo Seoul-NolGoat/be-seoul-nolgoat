d61e66f70403d4352ba61ef735754b2466dd85cf
yonghyeonpark <pyh4616@naver.com>
2024-11-25 17:54:23 +0900

    refactor: 메서드 이동 및 불필요한 메서드 삭제
    - 트랜잭션 사용을 위해 processOAuth2User 메서드를 CustomOAuth2UserService에서 UserService로 이동

--- src/main/java/wad/seoul_nolgoat/auth/oauth2/CustomOAuth2UserService.java
+++ src/main/java/wad/seoul_nolgoat/auth/oauth2/CustomOAuth2UserService.java
@@ -7,13 +7,13 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
-import wad.seoul_nolgoat.auth.oauth2.dto.*;
-import wad.seoul_nolgoat.domain.user.User;
-import wad.seoul_nolgoat.domain.user.UserRepository;
+import org.springframework.transaction.annotation.Transactional;
+import wad.seoul_nolgoat.auth.oauth2.dto.GoogleResponse;
+import wad.seoul_nolgoat.auth.oauth2.dto.KakaoResponse;
 import wad.seoul_nolgoat.exception.ApiException;
+import wad.seoul_nolgoat.service.user.UserService;
 
 import static wad.seoul_nolgoat.exception.ErrorCode.UNSUPPORTED_PROVIDER;
-import static wad.seoul_nolgoat.exception.ErrorCode.USER_NOT_FOUND;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -24,50 +24,21 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     public static final String GOOGLE = "google";
     public static final String PROVIDER_ID_DELIMITER = "_";
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
+    @Transactional
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
         if (registrationId.equals(KAKAO)) {
-            return processOAuth2User(new KakaoResponse(oAuth2User.getAttributes()));
+            return userService.processOAuth2User(new KakaoResponse(oAuth2User.getAttributes()));
         }
         if (registrationId.equals(GOOGLE)) {
-            return processOAuth2User(new GoogleResponse(oAuth2User.getAttributes()));
+            return userService.processOAuth2User(new GoogleResponse(oAuth2User.getAttributes()));
         }
         log.error("Unsupported OAuth2 provider: {}", registrationId);
         throw new ApiException(UNSUPPORTED_PROVIDER);
-    }
-
-    private OAuth2User processOAuth2User(OAuth2Response oAuth2Response) {
-        String uniqueProviderId = oAuth2Response.getProvider() + PROVIDER_ID_DELIMITER + oAuth2Response.getProviderId();
-        String nickname = oAuth2Response.getNickname();
-        String profileImage = oAuth2Response.getProfileImage();
-
-        if (!userRepository.existsByLoginId(uniqueProviderId)) {
-            userRepository.save(
-                    new User(
-                            uniqueProviderId,
-                            null,
-                            nickname,
-                            profileImage,
-                            null,
-                            null
-                    )
-            );
-            OAuth2UserDto oAuth2UserDto = new OAuth2UserDto(uniqueProviderId);
-            return new OAuth2UserImpl(oAuth2UserDto);
-        }
-        User user = userRepository.findByLoginId(uniqueProviderId)
-                .orElseThrow(() -> new ApiException(USER_NOT_FOUND));
-        user.update(
-                null,
-                nickname,
-                profileImage
-        );
-        OAuth2UserDto oAuth2UserDto = new OAuth2UserDto(uniqueProviderId);
-        return new OAuth2UserImpl(oAuth2UserDto);
     }
 }
