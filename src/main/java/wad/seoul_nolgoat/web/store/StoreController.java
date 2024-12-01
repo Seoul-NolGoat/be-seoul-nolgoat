0a9105e8f4773f06abdab66241291d55451820f3
yonghyeonpark <pyh4616@naver.com>
2024-12-02 05:19:02 +0900

    refactor: 가게 관련 스웨거 어노테이션 추가

--- src/main/java/wad/seoul_nolgoat/web/store/StoreController.java
+++ src/main/java/wad/seoul_nolgoat/web/store/StoreController.java
@@ -1,5 +1,7 @@
 package wad.seoul_nolgoat.web.store;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,7 @@ import wad.seoul_nolgoat.web.store.dto.response.StoreForBookmarkDto;
 
 import java.util.List;
 
+@Tag(name = "가게")
 @RequiredArgsConstructor
 @RequestMapping("/api/stores")
 @RestController
@@ -21,12 +24,14 @@ public class StoreController {
     private final StoreService storeService;
     private final BookmarkService bookmarkService;
 
+    @Operation(summary = "가게 단건 조회")
     @GetMapping("/{storeId}")
     public ResponseEntity<StoreDetailsDto> showStoreByStoreId(@PathVariable Long storeId) {
         return ResponseEntity
                 .ok(storeService.findByStoreId(storeId));
     }
 
+    @Operation(summary = "유저 ID를 통한 즐겨찾기 가게 목록 조회")
     @GetMapping("/bookmarked/{userId}")
     public ResponseEntity<List<StoreForBookmarkDto>> showBookmarkedStoresByUserId(@PathVariable Long userId) {
         return ResponseEntity
