8c1928dba5e09fd8fea30f52dcd5aa9f188079e0
yonghyeonpark <pyh4616@naver.com>
2024-12-02 05:17:13 +0900

    refactor: 공지 사항 관련 스웨거 어노테이션 추가

--- src/main/java/wad/seoul_nolgoat/web/notice/NoticeController.java
+++ src/main/java/wad/seoul_nolgoat/web/notice/NoticeController.java
@@ -1,5 +1,8 @@
 package wad.seoul_nolgoat.web.notice;
 
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,6 +18,7 @@ import wad.seoul_nolgoat.web.notice.dto.response.NoticeListDto;
 import java.net.URI;
 import java.util.List;
 
+@Tag(name = "공지 사항")
 @RequiredArgsConstructor
 @RequestMapping("/api/notices")
 @RestController
@@ -22,6 +26,7 @@ public class NoticeController {
 
     private final NoticeService noticeService;
 
+    @Operation(summary = "공지 사항 작성")
     @PostMapping
     public ResponseEntity<Void> createNotice(
             @AuthenticationPrincipal OAuth2User loginUser,
@@ -38,18 +43,21 @@ public class NoticeController {
                 .build();
     }
 
+    @Operation(summary = "공지 사항 단건 조회")
     @GetMapping("/{noticeId}")
     public ResponseEntity<NoticeDetailsDto> showNoticeByNoticeId(@PathVariable Long noticeId) {
         return ResponseEntity
                 .ok(noticeService.findByNoticeId(noticeId));
     }
 
+    @Operation(summary = "공지 사항 목록 조회")
     @GetMapping
     public ResponseEntity<List<NoticeListDto>> showAllNotices() {
         return ResponseEntity
                 .ok(noticeService.findAllNotice());
     }
 
+    @Operation(summary = "공지 사항 수정")
     @PutMapping("/{noticeId}")
     public ResponseEntity<Void> update(
             @PathVariable Long noticeId,
@@ -62,6 +70,7 @@ public class NoticeController {
                 .build();
     }
 
+    @Operation(summary = "공지 사항 삭제")
     @DeleteMapping("/{noticeId}")
     public ResponseEntity<Void> deleteById(@PathVariable Long noticeId) {
         noticeService.deleteById(noticeId);
@@ -71,6 +80,7 @@ public class NoticeController {
                 .build();
     }
 
+    @Hidden
     @PutMapping("/{noticeId}/views")
     public ResponseEntity<Void> increaseViews(@PathVariable Long noticeId) {
         noticeService.increaseViews(noticeId);
