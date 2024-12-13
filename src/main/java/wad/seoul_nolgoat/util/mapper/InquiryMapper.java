1837f09d872cc7ef928756c6f37f2f33cfae6205
yonghyeonpark <pyh4616@naver.com>
2024-12-13 19:55:48 +0900

    refactor: 불필요한 메서드 삭제

--- src/main/java/wad/seoul_nolgoat/util/mapper/InquiryMapper.java
+++ src/main/java/wad/seoul_nolgoat/util/mapper/InquiryMapper.java
@@ -4,7 +4,6 @@ import wad.seoul_nolgoat.domain.inquiry.Inquiry;
 import wad.seoul_nolgoat.domain.user.User;
 import wad.seoul_nolgoat.web.inquiry.dto.request.InquirySaveDto;
 import wad.seoul_nolgoat.web.inquiry.dto.response.InquiryDetailsDto;
-import wad.seoul_nolgoat.web.inquiry.dto.response.InquiryListDto;
 
 import static wad.seoul_nolgoat.util.DateTimeUtil.formatDate;
 
@@ -24,18 +23,6 @@ public class InquiryMapper {
                 inquiry.getId(),
                 inquiry.getTitle(),
                 inquiry.getContent(),
-                inquiry.getIsPublic(),
-                inquiry.getUser().getId(),
-                inquiry.getUser().getNickname(),
-                inquiry.getUser().getProfileImage(),
-                formatDate(inquiry.getCreatedDate())
-        );
-    }
-
-    public static InquiryListDto toInquiryListDto(Inquiry inquiry) {
-        return new InquiryListDto(
-                inquiry.getId(),
-                inquiry.getTitle(),
                 inquiry.getIsPublic(),
                 inquiry.getUser().getId(),
                 inquiry.getUser().getNickname(),
