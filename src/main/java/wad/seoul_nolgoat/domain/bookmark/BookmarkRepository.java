b74c71838c844f679c4bf0b5b70df8e7253b9bb9
yonghyeonpark <pyh4616@naver.com>
2024-12-14 04:31:50 +0900

    refactor: 불필요한 delete 메서드 삭제 및 findByUserIdAndStoreId 추가

--- src/main/java/wad/seoul_nolgoat/domain/bookmark/BookmarkRepository.java
+++ src/main/java/wad/seoul_nolgoat/domain/bookmark/BookmarkRepository.java
@@ -1,19 +1,15 @@
 package wad.seoul_nolgoat.domain.bookmark;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
     List<Bookmark> findByUserId(Long userId);
 
-    boolean existsByUserIdAndStoreId(Long userId, Long storeId);
+    Optional<Bookmark> findByUserIdAndStoreId(Long userId, Long storeId);
 
-    @Modifying
-    @Query("DELETE FROM Bookmark b WHERE b.user.id = :userId AND b.store.id = :storeId")
-    void deleteByUserIdAndStoreId(@Param("userId") Long userId, @Param("storeId") Long storeId);
+    boolean existsByUserIdAndStoreId(Long userId, Long storeId);
 }
