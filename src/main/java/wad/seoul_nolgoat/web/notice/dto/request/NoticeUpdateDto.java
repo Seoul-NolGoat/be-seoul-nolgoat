c6bb7d864f65591d26e3e1f6791132b0bf93b127
yonghyeonpark <pyh4616@naver.com>
2024-12-13 21:59:24 +0900

    refactor: 공지 사항 및 건의 사항 요청 DTO에 유효성 검증 추가

--- src/main/java/wad/seoul_nolgoat/web/notice/dto/request/NoticeUpdateDto.java
+++ src/main/java/wad/seoul_nolgoat/web/notice/dto/request/NoticeUpdateDto.java
@@ -1,5 +1,7 @@
 package wad.seoul_nolgoat.web.notice.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -7,7 +9,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class NoticeUpdateDto {
 
+    @NotBlank
+    @Size(max = 25, message = "제목은 25자 이내여야 합니다.")
     private final String title;
+
+    @NotBlank
+    @Size(max = 300, message = "내용은 300자 이내여야 합니다.")
     private final String content;
-    private final Boolean isPublic;
 }
