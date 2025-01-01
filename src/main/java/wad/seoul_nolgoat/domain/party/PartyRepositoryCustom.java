1addc9ef478e1fa6064de6858dcedc3b02cbf3c5
yonghyeonpark <pyh4616@naver.com>
2025-01-02 03:44:41 +0900

    feat: 내가 만든 파티 목록 조회 기능 구현

--- src/main/java/wad/seoul_nolgoat/domain/party/PartyRepositoryCustom.java
+++ src/main/java/wad/seoul_nolgoat/domain/party/PartyRepositoryCustom.java
@@ -1,10 +1,16 @@
 package wad.seoul_nolgoat.domain.party;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import wad.seoul_nolgoat.web.party.request.PartySearchConditionDto;
+import wad.seoul_nolgoat.web.party.response.HostedPartyListDto;
 import wad.seoul_nolgoat.web.party.response.PartyListDto;
 
 public interface PartyRepositoryCustom {
 
     Page<PartyListDto> findAllWithConditionAndPagination(PartySearchConditionDto partySearchConditionDto);
+
+    Page<HostedPartyListDto> findHostedPartiesByLoginId(String loginId, Pageable pageable);
+
+    Page<PartyListDto> findJoinedPartiesByLoginId(String loginId, Pageable pageable);
 }
