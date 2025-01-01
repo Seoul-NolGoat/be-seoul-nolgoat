bd35e13f9e4328cea985399aa8736f2afdfc8750
yonghyeonpark <pyh4616@naver.com>
2025-01-02 03:42:49 +0900

    feat: 내가 생성한 파티 목록 조회를 위한 DTO 구현

---
+++ src/main/java/wad/seoul_nolgoat/web/party/response/HostedPartyListDto.java
@@ -0,0 +1,36 @@
+package wad.seoul_nolgoat.web.party.response;
+
+import lombok.Getter;
+import wad.seoul_nolgoat.domain.party.AdministrativeDistrict;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class HostedPartyListDto {
+
+    private final Long partyId;
+    private final String title;
+    private final int maxCapacity;
+    private final LocalDateTime deadline;
+    private final boolean isClosed;
+    private final String district;
+    private final int currentCount;
+
+    public HostedPartyListDto(
+            Long partyId,
+            String title,
+            int maxCapacity,
+            LocalDateTime deadline,
+            boolean isClosed,
+            AdministrativeDistrict district,
+            Long currentCount
+    ) {
+        this.partyId = partyId;
+        this.title = title;
+        this.maxCapacity = maxCapacity;
+        this.deadline = deadline;
+        this.isClosed = isClosed;
+        this.district = district.getDisplayName();
+        this.currentCount = currentCount.intValue() + 1;
+    }
+}
