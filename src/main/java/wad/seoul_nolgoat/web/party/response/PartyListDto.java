d545ca78c3adc41dfb8f11a1a8b7d52fc5c97a9b
yonghyeonpark <pyh4616@naver.com>
2025-01-02 00:08:09 +0900

    feat: 파티 목록 조회에 대한 응답 DTO 구현

---
+++ src/main/java/wad/seoul_nolgoat/web/party/response/PartyListDto.java
@@ -0,0 +1,45 @@
+package wad.seoul_nolgoat.web.party.response;
+
+import lombok.Getter;
+import wad.seoul_nolgoat.domain.party.AdministrativeDistrict;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PartyListDto {
+
+    private final Long partyId;
+    private final String title;
+    private final int maxCapacity;
+    private final LocalDateTime deadline;
+    private final boolean isClosed;
+    private final String district;
+    private final int currentCount;
+    private final Long hostId;
+    private final String hostNickname;
+    private final String hostProfileImage;
+
+    public PartyListDto(
+            Long partyId,
+            String title,
+            int maxCapacity,
+            LocalDateTime deadline,
+            boolean isClosed,
+            AdministrativeDistrict district,
+            int currentCount,
+            Long hostId,
+            String hostNickname,
+            String hostProfileImage
+    ) {
+        this.partyId = partyId;
+        this.title = title;
+        this.maxCapacity = maxCapacity;
+        this.deadline = deadline;
+        this.isClosed = isClosed;
+        this.district = district.getDisplayName();
+        this.currentCount = currentCount;
+        this.hostId = hostId;
+        this.hostNickname = hostNickname;
+        this.hostProfileImage = hostProfileImage;
+    }
+}
