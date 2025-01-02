525b85d5227671729d9d8abdb21989a88666a22d
yonghyeonpark <pyh4616@naver.com>
2025-01-02 18:48:39 +0900

    refactor: 파티 단건 조회 응답 DTO 스펙 수정

--- src/main/java/wad/seoul_nolgoat/web/party/response/PartyDetailsDto.java
+++ src/main/java/wad/seoul_nolgoat/web/party/response/PartyDetailsDto.java
@@ -1,12 +1,12 @@
 package wad.seoul_nolgoat.web.party.response;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import wad.seoul_nolgoat.domain.party.AdministrativeDistrict;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
-@RequiredArgsConstructor
 public class PartyDetailsDto {
 
     private final Long id;
@@ -16,5 +16,40 @@ public class PartyDetailsDto {
     private final int maxCapacity;
     private final LocalDateTime deadline;
     private final boolean isClosed;
+    private final String district;
     private final int currentCount;
+    private final Long hostId;
+    private final String hostNickname;
+    private final String hostProfileImage;
+    private final List<ParticipantDto> participants;
+
+    public PartyDetailsDto(
+            Long id,
+            String title,
+            String content,
+            String imageUrl,
+            int maxCapacity,
+            LocalDateTime deadline,
+            boolean isClosed,
+            AdministrativeDistrict district,
+            Long currentCount,
+            Long hostId,
+            String hostNickname,
+            String hostProfileImage,
+            List<ParticipantDto> participants
+    ) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.imageUrl = imageUrl;
+        this.maxCapacity = maxCapacity;
+        this.deadline = deadline;
+        this.isClosed = isClosed;
+        this.district = district.getDisplayName();
+        this.currentCount = currentCount.intValue() + 1;
+        this.hostId = hostId;
+        this.hostNickname = hostNickname;
+        this.hostProfileImage = hostProfileImage;
+        this.participants = participants;
+    }
 }
