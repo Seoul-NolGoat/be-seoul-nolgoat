3408c3b0727ee672496ce134cf3fee8b0265ef53
yonghyeonpark <pyh4616@naver.com>
2025-01-03 03:44:23 +0900

    refactor: 사용하지 않는 메서드(비관적 락) 삭제

--- src/main/java/wad/seoul_nolgoat/domain/party/PartyRepository.java
+++ src/main/java/wad/seoul_nolgoat/domain/party/PartyRepository.java
@@ -1,15 +1,14 @@
 package wad.seoul_nolgoat.domain.party;
 
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface PartyRepository extends JpaRepository<Party, Long>, PartyRepositoryCustom {
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("SELECT p FROM Party p WHERE p.id = :id")
-    Optional<Party> findByIdWithLock(Long id);
+    @Query("SELECT p FROM Party p " +
+            "LEFT JOIN FETCH p.host " +
+            "WHERE p.id = :partyId")
+    Optional<Party> findByIdWithFetchJoin(Long partyId);
 }
