57fc90260712807919acf7f466a6b9f4086ef594
yonghyeonpark <pyh4616@naver.com>
2025-01-04 03:48:17 +0900

    refactor: 불필요한 메서드 삭제

--- src/main/java/wad/seoul_nolgoat/domain/party/PartyUserRepository.java
+++ src/main/java/wad/seoul_nolgoat/domain/party/PartyUserRepository.java
@@ -1,14 +1,10 @@
 package wad.seoul_nolgoat.domain.party;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface PartyUserRepository extends JpaRepository<PartyUser, Long> {
-
-    @Query("SELECT COUNT(pu) FROM PartyUser pu WHERE pu.party.id = :partyId")
-    int countByPartyId(Long partyId);
 
     boolean existsByPartyIdAndParticipantId(Long partyId, Long participantId);
 
