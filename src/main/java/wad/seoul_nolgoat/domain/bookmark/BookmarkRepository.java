73fd9c8b79a7b8bafb27cbcf4554d939b2b76811
yonghyeonpark <pyh4616@naver.com>
2025-01-07 22:13:18 +0900

    refactor: 불필요한 메서드 삭제

--- src/main/java/wad/seoul_nolgoat/domain/bookmark/BookmarkRepository.java
+++ src/main/java/wad/seoul_nolgoat/domain/bookmark/BookmarkRepository.java
@@ -2,12 +2,9 @@ package wad.seoul_nolgoat.domain.bookmark;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long>, BookmarkRepositoryCustom {
-
-    List<Bookmark> findByUserId(Long userId);
 
     Optional<Bookmark> findByUserIdAndStoreId(Long userId, Long storeId);
 
--- src/main/java/wad/seoul_nolgoat/domain/review/ReviewRepository.java
+++ src/main/java/wad/seoul_nolgoat/domain/review/ReviewRepository.java
@@ -2,11 +2,7 @@ package wad.seoul_nolgoat.domain.review;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositoryCustom {
-
-    List<Review> findByUserId(Long userId);
 
     boolean existsByUserIdAndStoreId(Long userId, Long storeId);
 }
