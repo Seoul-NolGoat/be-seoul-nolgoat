f60d5afe3faadc8ec3919529265a7a75f1406187
Daniel <923daniel@naver.com>
2025-01-12 03:12:05 +0900

    feat: 상점 검색 기능 구현

--- src/main/java/wad/seoul_nolgoat/web/store/StoreController.java
+++ src/main/java/wad/seoul_nolgoat/web/store/StoreController.java
@@ -3,13 +3,12 @@ package wad.seoul_nolgoat.web.store;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import wad.seoul_nolgoat.service.store.StoreService;
 import wad.seoul_nolgoat.web.store.dto.response.StoreDetailsDto;
+import wad.seoul_nolgoat.web.store.dto.response.StoreListDto;
 
 @Tag(name = "가게")
 @RequiredArgsConstructor
@@ -24,5 +23,14 @@ public class StoreController {
     public ResponseEntity<StoreDetailsDto> showStoreByStoreId(@PathVariable Long storeId) {
         return ResponseEntity
                 .ok(storeService.findStoreWithReviewsByStoreId(storeId));
+    }
+
+    @GetMapping("/search")
+    public Page<StoreListDto> searchStores(
+            @RequestParam String searchInput,
+            @RequestParam int page,
+            @RequestParam int size
+    ) {
+        return storeService.searchStores(searchInput, page, size);
     }
 }
