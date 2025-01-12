a805d49b8bfcff12fb62653a896a9de1d2038c1a
yonghyeonpark <pyh4616@naver.com>
2025-01-12 22:31:24 +0900

    refactor: 불필요한 메서드 삭제

--- src/main/java/wad/seoul_nolgoat/util/mapper/UserMapper.java
+++ src/main/java/wad/seoul_nolgoat/util/mapper/UserMapper.java
@@ -1,21 +1,9 @@
 package wad.seoul_nolgoat.util.mapper;
 
 import wad.seoul_nolgoat.domain.user.User;
-import wad.seoul_nolgoat.web.user.dto.request.UserSaveDto;
 import wad.seoul_nolgoat.web.user.dto.response.UserProfileDto;
 
 public class UserMapper {
-
-    public static User toEntity(UserSaveDto userSaveDto) {
-        return new User(
-                userSaveDto.getLoginId(),
-                userSaveDto.getPassword(),
-                userSaveDto.getNickname(),
-                userSaveDto.getProfileImage(),
-                userSaveDto.getGender(),
-                userSaveDto.getBirthDate()
-        );
-    }
 
     public static UserProfileDto toUserProfileDto(User user) {
         return new UserProfileDto(
