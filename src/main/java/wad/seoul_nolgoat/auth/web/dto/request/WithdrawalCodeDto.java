7a5b5ffba307edca0e1b8d701379663bbd20498d
yonghyeonpark <pyh4616@naver.com>
2025-01-13 20:05:55 +0900

    refactor: JSON 파싱이 가능하도록 어노테이션 추가

--- src/main/java/wad/seoul_nolgoat/auth/web/dto/request/WithdrawalCodeDto.java
+++ src/main/java/wad/seoul_nolgoat/auth/web/dto/request/WithdrawalCodeDto.java
@@ -1,11 +1,15 @@
 package wad.seoul_nolgoat.auth.web.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Getter
 public class WithdrawalCodeDto {
 
     private final String inputCode;
+
+    @JsonCreator
+    public WithdrawalCodeDto(String inputCode) {
+        this.inputCode = inputCode;
+    }
 }
