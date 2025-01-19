431a07a3f546b676856155b1ed26d470a355610b
yonghyeonpark <pyh4616@naver.com>
2025-01-19 15:56:27 +0900

    feat: 요청 처리 시간과 상태 기록을 위한 로깅 필터 구현

---
+++ src/main/java/wad/seoul_nolgoat/filter/RequestLoggingFilter.java
@@ -0,0 +1,27 @@
+package wad.seoul_nolgoat.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+public class RequestLoggingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        long startTime = System.currentTimeMillis();
+
+        log.info("Request received");
+        try {
+            filterChain.doFilter(request, response);
+        } finally {
+            long duration = System.currentTimeMillis() - startTime;
+            log.info("Request processed [Status:{}] [Duration:{}ms]", response.getStatus(), duration);
+        }
+    }
+}
