60e635a14e166c1ffcbefa39c725b1c1ac8aa0e6
yonghyeonpark <pyh4616@naver.com>
2025-01-20 22:02:15 +0900

    feat: Comment 엔티티 클래스 구현

---
+++ src/main/java/wad/seoul_nolgoat/domain/comment/Comment.java
@@ -0,0 +1,32 @@
+package wad.seoul_nolgoat.domain.comment;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import wad.seoul_nolgoat.domain.party.Party;
+import wad.seoul_nolgoat.domain.user.User;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User writer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Party party;
+
+    private String content;
+
+    public Comment(User writer, Party party, String content) {
+        this.writer = writer;
+        this.party = party;
+        this.content = content;
+    }
+}
