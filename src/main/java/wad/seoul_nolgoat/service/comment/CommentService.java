05915c29d0ddd4efc51a6358d95329c9de531764
yonghyeonpark <pyh4616@naver.com>
2025-01-20 22:45:29 +0900

    feat: 댓글 수정 API 구현

--- src/main/java/wad/seoul_nolgoat/service/comment/CommentService.java
+++ src/main/java/wad/seoul_nolgoat/service/comment/CommentService.java
@@ -2,6 +2,7 @@ package wad.seoul_nolgoat.service.comment;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import wad.seoul_nolgoat.domain.comment.Comment;
 import wad.seoul_nolgoat.domain.comment.CommentRepository;
 import wad.seoul_nolgoat.domain.party.Party;
 import wad.seoul_nolgoat.domain.party.PartyRepository;
@@ -10,9 +11,9 @@ import wad.seoul_nolgoat.domain.user.UserRepository;
 import wad.seoul_nolgoat.exception.ApplicationException;
 import wad.seoul_nolgoat.util.mapper.CommentMapper;
 import wad.seoul_nolgoat.web.comment.dto.CommentSaveDto;
+import wad.seoul_nolgoat.web.comment.dto.CommentUpdateDto;
 
-import static wad.seoul_nolgoat.exception.ErrorCode.PARTY_NOT_FOUND;
-import static wad.seoul_nolgoat.exception.ErrorCode.USER_NOT_FOUND;
+import static wad.seoul_nolgoat.exception.ErrorCode.*;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +36,20 @@ public class CommentService {
 
         return commentRepository.save(CommentMapper.toEntity(commentSaveDto, user, party))
                 .getId();
+    }
+
+    public void update(
+            String loginId,
+            CommentUpdateDto commentUpdateDto,
+            Long commentId
+    ) {
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new ApplicationException(COMMENT_NOT_FOUND));
+
+        if (!comment.getWriter().getLoginId().equals(loginId)) {
+            throw new ApplicationException(COMMENT_UPDATE_NOT_AUTHORIZED);
+        }
+
+        comment.update(commentUpdateDto.getContent());
     }
 }
