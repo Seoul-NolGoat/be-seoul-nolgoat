fb564ab54f79fceff7a85e390e0e82d4bb5ef8ca
yonghyeonpark <pyh4616@naver.com>
2025-01-22 04:32:25 +0900

    feat: 비관적 락을 적용한 파티 및 호스트 조회 쿼리 메서드 구현

--- src/main/java/wad/seoul_nolgoat/domain/party/PartyRepository.java
+++ src/main/java/wad/seoul_nolgoat/domain/party/PartyRepository.java
@@ -1,6 +1,8 @@
 package wad.seoul_nolgoat.domain.party;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
@@ -8,7 +10,14 @@ import java.util.Optional;
 public interface PartyRepository extends JpaRepository<Party, Long>, PartyRepositoryCustom {
 
     @Query("SELECT p FROM Party p " +
-            "LEFT JOIN FETCH p.host " +
+            "JOIN FETCH p.host " +
             "WHERE p.id = :partyId")
     Optional<Party> findByIdWithFetchJoin(Long partyId);
+
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT p FROM Party p " +
+            "JOIN FETCH p.host " +
+            "WHERE p.id = :partyId")
+    Optional<Party> findByIdWithFetchJoinAndLock(Long id);
 }
