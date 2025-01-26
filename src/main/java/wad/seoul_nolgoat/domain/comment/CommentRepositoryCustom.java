77da66a08ffa0153d96ba6488a76dea2b31cfba9
yonghyeonpark <llyyoo93@gmail.com>
2025-01-27 04:19:39 +0900

    feat: partyId로 해당 파티의 댓글 목록을 조회하는 기능 구현

--- src/main/java/wad/seoul_nolgoat/domain/comment/CommentRepositoryCustom.java
+++ src/main/java/wad/seoul_nolgoat/domain/comment/CommentRepositoryCustom.java
@@ -2,9 +2,14 @@ package wad.seoul_nolgoat.domain.comment;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import wad.seoul_nolgoat.web.comment.dto.response.CommentDetailsForPartyDto;
 import wad.seoul_nolgoat.web.comment.dto.response.CommentDetailsForUserDto;
+
+import java.util.List;
 
 public interface CommentRepositoryCustom {
 
     Page<CommentDetailsForUserDto> findCommentsByLoginId(String loginId, Pageable pageable);
+
+    List<CommentDetailsForPartyDto> findCommentsByPartyId(Long partyId);
 }
