0dd17020573df611f9d0fb21cbf8c95932418f83
yonghyeonpark <llyyoo93@gmail.com>
2025-01-26 15:34:22 +0900

    refactor: Party 엔티티를 PartyDetailsForListDto로 변환하는 메서드 추가

--- src/main/java/wad/seoul_nolgoat/web/party/response/PartyDetailsForListDto.java
+++ src/main/java/wad/seoul_nolgoat/web/party/response/PartyDetailsForListDto.java
@@ -2,6 +2,7 @@ package wad.seoul_nolgoat.web.party.response;
 
 import lombok.Getter;
 import wad.seoul_nolgoat.domain.party.AdministrativeDistrict;
+import wad.seoul_nolgoat.domain.party.Party;
 
 import java.time.LocalDateTime;
 
@@ -20,7 +21,7 @@ public class PartyDetailsForListDto {
     private final String hostNickname;
     private final String hostProfileImage;
 
-    public PartyDetailsForListDto(
+    private PartyDetailsForListDto(
             Long partyId,
             String title,
             int maxCapacity,
@@ -44,5 +45,21 @@ public class PartyDetailsForListDto {
         this.hostId = hostId;
         this.hostNickname = hostNickname;
         this.hostProfileImage = hostProfileImage;
+    }
+
+    public static PartyDetailsForListDto from(Party party) {
+        return new PartyDetailsForListDto(
+                party.getId(),
+                party.getTitle(),
+                party.getMaxCapacity(),
+                party.getMeetingDate(),
+                party.isClosed(),
+                party.getAdministrativeDistrict(),
+                party.getCurrentCount(),
+                party.getCreatedDate(),
+                party.getHost().getId(),
+                party.getHost().getNickname(),
+                party.getHost().getProfileImage()
+        );
     }
 }
