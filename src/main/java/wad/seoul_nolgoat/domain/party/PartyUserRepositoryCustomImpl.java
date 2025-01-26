5f5653bb8cacfa81b2d24fe6cc7c45ace8625b2f
yonghyeonpark <llyyoo93@gmail.com>
2025-01-27 04:58:29 +0900

    feat: partyId를 통해 해당 파티의 참여자 목록을 가져오는 기능 구현

---
+++ src/main/java/wad/seoul_nolgoat/domain/party/PartyUserRepositoryCustomImpl.java
@@ -0,0 +1,36 @@
+package wad.seoul_nolgoat.domain.party;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import wad.seoul_nolgoat.web.party.response.ParticipantDto;
+
+import java.util.List;
+
+import static wad.seoul_nolgoat.domain.party.QPartyUser.partyUser;
+
+@RequiredArgsConstructor
+public class PartyUserRepositoryCustomImpl implements PartyUserRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public List<ParticipantDto> findParticipantsByPartyId(Long partyId) {
+        jpaQueryFactory
+                .select(
+                        Projections.constructor(
+                                ParticipantDto.class,
+                                partyUser.participant.id,
+                                partyUser.participant.nickname,
+                                partyUser.participant.profileImage,
+                                partyUser.participant.loginId
+                        )
+                )
+                .from(partyUser)
+                .where(partyUser.party.id.eq(partyId))
+                .orderBy(partyUser.createdDate.asc())
+                .fetch();
+
+        return List.of();
+    }
+}
