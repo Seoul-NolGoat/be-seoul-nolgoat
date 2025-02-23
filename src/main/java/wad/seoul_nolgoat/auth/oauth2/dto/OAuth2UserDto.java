afb34bdb4fa23603e5f006542f47c9211bd202ea
yonghyeonpark <llyyoo93@gmail.com>
2025-02-23 19:37:05 +0900

    refactor: 인증 관련 DTO 클래스를 record로 수정

--- src/main/java/wad/seoul_nolgoat/auth/oauth2/dto/OAuth2UserDto.java
+++ src/main/java/wad/seoul_nolgoat/auth/oauth2/dto/OAuth2UserDto.java
@@ -1,11 +1,4 @@
 package wad.seoul_nolgoat.auth.oauth2.dto;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
-public class OAuth2UserDto {
-
-    private final String uniqueProviderId;
+public record OAuth2UserDto(String uniqueProviderId) {
 }
