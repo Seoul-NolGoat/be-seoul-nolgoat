420fbd17974b43731dff9a03cf13f396b14f91b8
yonghyeonpark <llyyoo93@gmail.com>
2025-02-26 06:01:14 +0900

    feat: 동시성, 기타 예외 관련 에러 코드 추가

--- src/main/java/wad/seoul_nolgoat/exception/ErrorCode.java
+++ src/main/java/wad/seoul_nolgoat/exception/ErrorCode.java
@@ -56,6 +56,7 @@ public enum ErrorCode {
     INVALID_MAX_CAPACITY(HttpStatus.BAD_REQUEST, "PARTY014", "참여 가능 인원은 현재 인원보다 적을 수 없습니다."),
     PARTY_COUNT_BELOW_MINIMUM(HttpStatus.BAD_REQUEST, "PARTY015", "현재 인원수는 1보다 작을 수 없습니다."),
     PARTY_CREATOR_CANNOT_LEAVE(HttpStatus.BAD_REQUEST, "PARTY016", "본인이 생성한 파티에서 탈퇴 할 수 없습니다."),
+    PARTY_CONCURRENT_ERROR(HttpStatus.CONFLICT, "PARTY017", "문제가 발생했습니다. 잠시 후 다시 시도해주세요."),
 
     // 댓글 관련
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMENT001", "존재하지 않는 댓글입니다."),
@@ -92,7 +93,10 @@ public enum ErrorCode {
 
     // 메일 관련
     MAIL_SEND_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "MAIL001", "메일 발송에 실패했습니다."),
-    MAIL_SENDER_ENCODING_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "MAIL002", "메일 발신자 정보 인코딩 중 오류가 발생했습니다");
+    MAIL_SENDER_ENCODING_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "MAIL002", "메일 발신자 정보 인코딩 중 오류가 발생했습니다"),
+
+    // 시스템 관련
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "SYS001", "알 수 없는 문제가 발생했습니다. 잠시 후 다시 시도해주세요.");
 
     private final HttpStatus httpStatus;
     private final String code;
