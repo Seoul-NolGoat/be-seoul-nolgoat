568478a7b9d8c9d81912bb35bb853c83821f68f5
yonghyeonpark <llyyoo93@gmail.com>
2025-03-16 04:31:02 +0900

    fix: 잘못된 어노테이션 수정

--- src/main/java/wad/seoul_nolgoat/web/inquiry/dto/request/InquirySaveDto.java
+++ src/main/java/wad/seoul_nolgoat/web/inquiry/dto/request/InquirySaveDto.java
@@ -1,6 +1,7 @@
 package wad.seoul_nolgoat.web.inquiry.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record InquirySaveDto(
@@ -8,7 +9,7 @@ public record InquirySaveDto(
         String title,
         @NotBlank @Size(max = 300, message = "내용은 300자 이내여야 합니다.")
         String content,
-        @NotBlank
+        @NotNull
         Boolean isPublic
 ) {
 }
